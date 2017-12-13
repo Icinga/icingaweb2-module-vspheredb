f91f8fbffdd846d3ed636eb1ce3d1edadc193775
Thomas Gelf <thomas@gelf.net>
2017-12-13 17:01:45 +0100

    VcenterServer: new object

---
+++ library/Vspheredb/DbObject/VcenterServer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\DbObject;
+
+class VcenterServer extends BaseDbObject
+{
+    protected $table = 'vcenter_server';
+
+    protected $autoincKeyName = 'id';
+
+    protected $defaultProperties = [
+        'id'              => null,
+        'vcenter_id'      => null,
+        'scheme'          => null,
+        'host'            => null,
+        'username'        => null,
+        'password'        => null,
+        'proxy_type'      => null,
+        'proxy_address'   => null,
+        'proxy_user'      => null,
+        'proxy_pass'      => null,
+        'ssl_verify_peer' => null,
+        'ssl_verify_host' => null,
+    ];
+}
