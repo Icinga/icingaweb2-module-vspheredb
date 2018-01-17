f3ee222fb19e4ddd3387807dc4cb018be5e53e19
Thomas Gelf <thomas@gelf.net>
2018-01-17 16:24:44 +0100

    Util: add binary UUID helpers

--- library/Vspheredb/Util.php
+++ library/Vspheredb/Util.php
@@ -17,6 +17,25 @@ class Util
         return $objects;
     }
 
+    public static function uuidToBin($uuid)
+    {
+        // 401daca3-42cf-bd89-94a1-463e448ea8d1
+        return hex2bin(str_replace('-', '', $uuid));
+    }
+
+    public static function uuidToHex($bin)
+    {
+        $hex = bin2hex($bin);
+
+        return implode('-', [
+            substr($hex, 0, 8),
+            substr($hex, 8, 4),
+            substr($hex, 12, 4),
+            substr($hex, 16, 4),
+            substr($hex, 20, 12)
+        ]);
+    }
+
     public static function extractNumericId($textualId, $optional = false)
     {
         if (is_object($textualId) && property_exists($textualId, '_')) {
