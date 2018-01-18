9d314883de7c5c8444980b02d87578a8527bec2e
Thomas Gelf <thomas@gelf.net>
2018-01-18 16:37:49 +0100

    SyncCommand: throw away old logic, use new Sync...
    ...implementations

--- application/clicommands/SyncCommand.php
+++ application/clicommands/SyncCommand.php
@@ -2,97 +2,39 @@
 
 namespace Icinga\Module\Vspheredb\Clicommands;
 
-use Icinga\Application\Benchmark;
-use Icinga\Module\Vspheredb\Api;
-use Icinga\Module\Vspheredb\DbObject\Datastore;
-use Icinga\Module\Vspheredb\DbObject\VmConfig;
-use Icinga\Module\Vspheredb\DbObject\HostSystem;
+use Icinga\Module\Vspheredb\Sync\Sync;
+use Icinga\Module\Vspheredb\Sync\SyncPerfCounterInfo;
+use Icinga\Module\Vspheredb\Sync\SyncPerfCounters;
 
 /**
  * Sync a vCenter or ESXi host
  */
 class SyncCommand extends CommandBase
 {
-    /** @var Api */
-    protected $api;
-
-    /** @var VmConfig[] */
-    protected $vms;
-
-    /** @var HostSystem[] */
-    protected $hosts;
-
     /**
-     * Sync Test
+     * Sync all objects
      *
-     * Mostly for test/debug reasons. Output occurs with default properties
+     * Still a prototype
      *
      * USAGE
      *
-     * icingacli vsphere sync test \
-     *     --vhost <vcenter> \
-     *     --username <username> \
-     *     --password <password> \
-     *     [options]
-     *
-     * OPTIONS
-     *
-     *   --lookup-ids             Replace id-references with their name
-     *                            This requires one additional API request
-     *   --no-ssl-verify-peer     Accept certificates signed by unknown CA
-     *   --no-ssl-verify-host     Accept certificates not matching the host
-     *   --use-insecure-http      Use plaintext HTTP requests
-     *   --proxy <proxy>          Use the given Proxy (ip, host or host:port)
-     *   --proxy-type <type>      HTTP (default) or SOCKS5
-     *   --proxy-username <user>  Username for authenticated HTTP proxy
-     *   --proxy-password <pass>  Password for authenticated HTTP proxy
-     *   --benchmark              Show resource usage summary
-     *   --json                   Dump JSON output
+     * icingacli vsphere sync objects --server_id <id>
      */
-    public function testAction()
+    public function objectsAction()
     {
-        $cnt = 0;
-        echo $this->screen->clear();
-        while (true) {
-            $cnt++;
-            $this->login();
-            $this->syncObjects();
-            HostSystem::syncFromApi($this->api(), $this->db());
-            VmConfig::syncFromApi($this->api(), $this->db());
-            Datastore::syncFromApi($this->api(), $this->db());
-            echo $this->screen->clear();
-            Benchmark::dump();
-            Benchmark::reset();
-            if ($cnt > 0) {
-                break;
-            } else {
-                sleep(10);
-            }
-        }
-
-        $this->logout();
+        $sync = new Sync($this->api(), $this->db());
+        $sync->syncAllObjects();
     }
 
-    protected function login()
+    public function perfcountersAction()
     {
-        Benchmark::measure('Preparing the API');
-        $api = $this->api();
-        $api->login();
-        $this->api = $api;
-        Benchmark::measure('Logged in');
+        $sync = new SyncPerfCounters($this->api(), $this->db());
+        $sync->run();
     }
 
-    protected function syncObjects()
+    public function perfcounterinfoAction()
     {
-        Benchmark::measure('Refreshing objects');
-        $this->api->idLookup($this->db())->refresh();
-        Benchmark::measure('Refreshed objects');
-    }
-
-    protected function logout()
-    {
-        Benchmark::measure('Trying to log out');
-        $this->api->logout();
-        Benchmark::measure('Logging out');
+        $sync = new SyncPerfCounterInfo($this->api(), $this->db());
+        $sync->run();
     }
 }
