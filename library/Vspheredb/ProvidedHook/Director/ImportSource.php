a095e883fcdddc149443c525fe94e83b9443778e
Thomas Gelf <thomas@gelf.net>
2018-01-18 16:45:36 +0100

    ImportSource: remove logic for now, prepare...
    ...VcenterServer form

--- library/Vspheredb/ProvidedHook/Director/ImportSource.php
+++ library/Vspheredb/ProvidedHook/Director/ImportSource.php
@@ -5,7 +5,6 @@ namespace Icinga\Module\Vspheredb\ProvidedHook\Director;
 use Icinga\Module\Director\Hook\ImportSourceHook;
 use Icinga\Module\Director\Web\Form\QuickForm;
 use Icinga\Module\Vspheredb\Api;
-use Icinga\Module\Vspheredb\Util;
 
 /**
  * Class ImportSource
@@ -19,17 +18,12 @@ class ImportSource extends ImportSourceHook
 
     public function getName()
     {
-        return 'VMware vSphere';
+        return 'VMware vSphereDB';
     }
 
     public function fetchData()
     {
-        $api = $this->api();
-        $api->login();
-        $objects = $this->callOnManagedObject('fetchWithDefaults', $api);
-        $api->idLookup()->enrichObjects($objects);
-        $api->logout();
-        return Util::createNestedObjects($objects);
+        return [];
     }
 
     public function listColumns()
@@ -39,7 +33,7 @@ class ImportSource extends ImportSourceHook
 
     protected function getManagedObjectClass()
     {
-        return 'Icinga\\Module\\Vspheredb\\ManagedObject\\'
+        return 'Icinga\\Module\\Vspheredb\\DbObject\\'
             . $this->getSetting('object_type');
     }
 
@@ -54,43 +48,6 @@ class ImportSource extends ImportSourceHook
         ), $params);
     }
 
-    protected function api()
-    {
-        if ($this->api === null) {
-            $scheme = $this->getSetting('scheme', 'HTTPS');
-            $this->api = new Api(
-                $this->getSetting('host'),
-                $this->getSetting('username'),
-                $this->getSetting('password'),
-                $scheme
-            );
-            $curl = $this->api->curl();
-
-            if ($proxy = $this->getSetting('proxy')) {
-                if ($proxyType = $this->getSetting('proxy_type')) {
-                    $curl->setProxy($proxy, $proxyType);
-                } else {
-                    $curl->setProxy($proxy);
-                }
-
-                if ($user = $this->getSetting('proxy_user')) {
-                    $curl->setProxyAuth($user, $this->getSetting('proxy_pass'));
-                }
-            }
-
-            if ($scheme === 'HTTPS') {
-                if ($this->getSetting('ssl_verify_peer', 'y') === 'n') {
-                    $curl->disableSslPeerVerification();
-                }
-                if ($this->getSetting('ssl_verify_host', 'y') === 'n') {
-                    $curl->disableSslHostVerification();
-                }
-            }
-        }
-
-        return $this->api;
-    }
-
     /**
      * @inheritdoc
      */
@@ -101,141 +58,5 @@ class ImportSource extends ImportSourceHook
 
     public static function addSettingsFormFields(QuickForm $form)
     {
-        if (! class_exists('SoapClient')) {
-            $form->addError($form->translate(
-                'The PHP SOAP extension (php-soap) is not installed/enabled'
-            ));
-
-            return;
-        }
-
-        $form->addElement('select', 'object_type', array(
-            'label' => $form->translate('Object Type'),
-            'description' => $form->translate(
-                'The managed vSphere object type this Import Source should fetch'
-            ),
-            'multiOptions' => $form->optionalEnum(array(
-                'VirtualMachine' => 'Virtual Machines',
-                'HostSystem'     => 'Host Systems',
-            )),
-            'required' => true,
-        ));
-
-        $form->addElement('select', 'scheme', array(
-            'label' => $form->translate('Protocol'),
-            'description' => $form->translate(
-                'Whether to use encryption when talking to your vCenter'
-            ),
-            'multiOptions' => array(
-                'HTTPS' => $form->translate('HTTPS (strongly recommended)'),
-                'HTTP'  => $form->translate('HTTP (this is plaintext!)'),
-            ),
-            'class' => 'autosubmit',
-            'value' => 'HTTPS',
-            'required' => true,
-        ));
-
-        $ssl = ! ($form->getSentOrObjectSetting('scheme', 'HTTPS') === 'HTTP');
-
-        if ($ssl) {
-            static::addBoolean($form, 'ssl_verify_peer', array(
-                'label'       => $form->translate('Verify Peer'),
-                'description' => $form->translate(
-                    'Whether we should check that our peer\'s certificate has'
-                    . ' been signed by a trusted CA. This is strongly recommended.'
-                )
-            ), 'y');
-            static::addBoolean($form, 'ssl_verify_host', array(
-                'label'       => $form->translate('Verify Host'),
-                'description' => $form->translate(
-                    'Whether we should check that the certificate matches the'
-                    . 'configured host'
-                )
-            ), 'y');
-        }
-
-        $form->addElement('text', 'host', array(
-            'label' => $form->translate('vCenter (or ESX) host'),
-            'description' => $form->translate(
-                'It is strongly suggested to access the API through your vCenter.'
-                . ' Usually this is just a fully qualified domain name (and should'
-                . ' match it\'s certificate. Alternatively, an IP address is fine.'
-                . ' Please use <host>:<port> in case you\'re not using default'
-                . ' HTTP(s) ports'
-            ),
-            'required' => true,
-        ));
-
-        $form->addElement('text', 'username', array(
-            'label' => $form->translate('Username'),
-            'description' => $form->translate(
-                'Will be used for SOAP authentication against your vCenter'
-            ),
-            'required' => true,
-        ));
-
-        $form->addElement('password', 'password', array(
-            'label' => $form->translate('Password'),
-            'required' => true,
-        ));
-
-        $form->addElement('select', 'proxy_type', array(
-            'label' => $form->translate('Proxy'),
-            'description' => $form->translate(
-                'In case your vCenter is only reachable through a proxy, please'
-                . ' choose it\'s protocol right here'
-            ),
-            'multiOptions' => $form->optionalEnum(array(
-                'HTTP'   => $form->translate('HTTP proxy'),
-                'SOCKS5' => $form->translate('SOCKS5 proxy'),
-            )),
-            'class' => 'autosubmit'
-        ));
-
-        $proxyType = $form->getSentOrObjectSetting('proxy_type');
-
-        if ($proxyType) {
-            $form->addElement('text', 'proxy', array(
-                'label' => $form->translate('Proxy Address'),
-                'description' => $form->translate(
-                    'Hostname, IP or <host>:<port>'
-                ),
-                'required' => true,
-            ));
-            if ($proxyType === 'HTTP') {
-                $form->addElement('text', 'proxy_user', array(
-                    'label' => $form->translate('Proxy Username'),
-                    'description' => $form->translate(
-                        'In case your proxy requires authentication, please'
-                        . ' configure this here'
-                    ),
-                ));
-
-                $passRequired = strlen($form->getSentOrObjectSetting('proxy_user')) > 0;
-
-                $form->addElement('password', 'proxy_pass', array(
-                    'label' => $form->translate('Proxy Password'),
-                    'required' => $passRequired
-                ));
-            }
-        }
-    }
-
-    protected static function addBoolean($form, $key, $options, $default = null)
-    {
-        if ($default === null) {
-            return $form->addElement('OptionalYesNo', $key, $options);
-        } else {
-            $form->addElement('YesNo', $key, $options);
-            return $form->getElement($key)->setValue($default);
-        }
-    }
-
-    protected static function optionalBoolean($form, $key, $label, $description)
-    {
-        return static::addBoolean($form, $key, array(
-            'label'       => $label,
-            'description' => $description
-        ));
     }
 }
---
+++ library/Vspheredb/Web/Form/VcenterServerForm.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Web\Form;
+
+use Icinga\Module\Director\Web\Form\QuickForm;
+
+class VcenterServerForm extends QuickForm
+{
+    public function setup()
+    {
+        if (! class_exists('SoapClient')) {
+            $this->addError($this->translate(
+                'The PHP SOAP extension (php-soap) is not installed/enabled'
+            ));
+
+            return;
+        }
+
+        $this->addElement('select', 'object_type', array(
+            'label' => $this->translate('Object Type'),
+            'description' => $this->translate(
+                'The managed vSphere object type this Import Source should fetch'
+            ),
+            'multiOptions' => $this->optionalEnum(array(
+                'VirtualMachine' => 'Virtual Machines',
+                'HostSystem'     => 'Host Systems',
+            )),
+            'required' => true,
+        ));
+
+        $this->addElement('select', 'scheme', array(
+            'label' => $this->translate('Protocol'),
+            'description' => $this->translate(
+                'Whether to use encryption when talking to your vCenter'
+            ),
+            'multiOptions' => array(
+                'HTTPS' => $this->translate('HTTPS (strongly recommended)'),
+                'HTTP'  => $this->translate('HTTP (this is plaintext!)'),
+            ),
+            'class' => 'autosubmit',
+            'value' => 'HTTPS',
+            'required' => true,
+        ));
+
+        $ssl = ! ($this->getSentOrObjectSetting('scheme', 'HTTPS') === 'HTTP');
+
+        if ($ssl) {
+            $this->addBoolean('ssl_verify_peer', array(
+                'label'       => $this->translate('Verify Peer'),
+                'description' => $this->translate(
+                    'Whether we should check that our peer\'s certificate has'
+                    . ' been signed by a trusted CA. This is strongly recommended.'
+                )
+            ), 'y');
+            $this->addBoolean('ssl_verify_host', array(
+                'label'       => $this->translate('Verify Host'),
+                'description' => $this->translate(
+                    'Whether we should check that the certificate matches the'
+                    . 'configured host'
+                )
+            ), 'y');
+        }
+
+        $this->addElement('text', 'host', array(
+            'label' => $this->translate('vCenter (or ESX) host'),
+            'description' => $this->translate(
+                'It is strongly suggested to access the API through your vCenter.'
+                . ' Usually this is just a fully qualified domain name (and should'
+                . ' match it\'s certificate. Alternatively, an IP address is fine.'
+                . ' Please use <host>:<port> in case you\'re not using default'
+                . ' HTTP(s) ports'
+            ),
+            'required' => true,
+        ));
+
+        $this->addElement('text', 'username', array(
+            'label' => $this->translate('Username'),
+            'description' => $this->translate(
+                'Will be used for SOAP authentication against your vCenter'
+            ),
+            'required' => true,
+        ));
+
+        $this->addElement('password', 'password', array(
+            'label' => $this->translate('Password'),
+            'required' => true,
+        ));
+
+        $this->addElement('select', 'proxy_type', array(
+            'label' => $this->translate('Proxy'),
+            'description' => $this->translate(
+                'In case your vCenter is only reachable through a proxy, please'
+                . ' choose it\'s protocol right here'
+            ),
+            'multiOptions' => $this->optionalEnum(array(
+                'HTTP'   => $this->translate('HTTP proxy'),
+                'SOCKS5' => $this->translate('SOCKS5 proxy'),
+            )),
+            'class' => 'autosubmit'
+        ));
+
+        $proxyType = $this->getSentOrObjectSetting('proxy_type');
+
+        if ($proxyType) {
+            $this->addElement('text', 'proxy', array(
+                'label' => $this->translate('Proxy Address'),
+                'description' => $this->translate(
+                    'Hostname, IP or <host>:<port>'
+                ),
+                'required' => true,
+            ));
+            if ($proxyType === 'HTTP') {
+                $this->addElement('text', 'proxy_user', array(
+                    'label' => $this->translate('Proxy Username'),
+                    'description' => $this->translate(
+                        'In case your proxy requires authentication, please'
+                        . ' configure this here'
+                    ),
+                ));
+
+                $passRequired = strlen($this->getSentOrObjectSetting('proxy_user')) > 0;
+
+                $this->addElement('password', 'proxy_pass', array(
+                    'label' => $this->translate('Proxy Password'),
+                    'required' => $passRequired
+                ));
+            }
+        }
+    }
+
+    // Are these needed?
+    protected function addBoolean($key, $options, $default = null)
+    {
+        if ($default === null) {
+            return $this->addElement('OptionalYesNo', $key, $options);
+        } else {
+            $this->addElement('YesNo', $key, $options);
+            return $this->getElement($key)->setValue($default);
+        }
+    }
+
+    protected function optionalBoolean($key, $label, $description)
+    {
+        return $this->addBoolean($key, [
+            'label'       => $label,
+            'description' => $description
+        ]);
+    }
+}
