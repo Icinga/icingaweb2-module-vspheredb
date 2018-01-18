027ed5ca7ac75fbe55ab66606ddb8c784b4ac850
Thomas Gelf <thomas@gelf.net>
2018-01-18 13:52:17 +0100

    OverviewController: cleanup, use new tables

--- application/controllers/OverviewController.php
+++ application/controllers/OverviewController.php
@@ -2,12 +2,13 @@
 
 namespace Icinga\Module\Vspheredb\Controllers;
 
-use Icinga\Module\Vspheredb\PathLookup;
 use Icinga\Module\Vspheredb\Web\Controller;
 use Icinga\Module\Vspheredb\Web\OverviewTree;
-use Icinga\Module\Vspheredb\Web\Table\DatastoreInFolderTable;
-use Icinga\Module\Vspheredb\Web\Table\HostsInFolderTable;
-use Icinga\Module\Vspheredb\Web\Table\VmsInFolderTable;
+use Icinga\Module\Vspheredb\Web\Table\Objects\DatastoreTable;
+use Icinga\Module\Vspheredb\Web\Table\Objects\HostsTable;
+use Icinga\Module\Vspheredb\Web\Table\Objects\ObjectsTable;
+use Icinga\Module\Vspheredb\Web\Table\Objects\VmsTable;
+use Icinga\Module\Vspheredb\Web\Table\Objects\VmsInFolderTable;
 use dipl\Html\Html;
 use dipl\Html\Link;
 
@@ -15,125 +16,148 @@ class OverviewController extends Controller
 {
     public function indexAction()
     {
-        $this
-            ->addSingleTab($this->translate('vSphere'))
-            ->addTitle('vSphere Overview');
-        $this->content()->add(
-            new OverviewTree($this->db(), $this->params->get('type'))
-        );
+        $type = $this->params->getRequired('type');
+        $this->activateTab($type)
+             ->addTitle('vSphere Overview')
+             ->content()->add(new OverviewTree($this->db(), $type));
     }
 
     public function hostsAction()
     {
-        $parent = $this->params->getRequired('id');
-        $lookup = new PathLookup($this->db());
-        $name = $lookup->getObjectName($parent);
-        $ids = $lookup->listFoldersBelongingTo($parent);
-        $this
-            ->addSingleTab($this->translate('Hosts'))
-            ->addTitle($name);
-        $this->actions()->add(
-            Link::create(
-                $this->translate('back'),
-                'vspheredb/overview',
-                null,
-                [
-                    'data-base-target' => '_main',
-                    'class' => 'icon-left-big'
-                ]
-            )
+        $this->addSingleTab($this->translate('Hosts'));
+        $this->linkBackToOverview('host');
+
+        $table = $this->eventuallyFilterByParent(
+            new HostsTable($this->db()),
+            'vspheredb/overview/hosts',
+            $this->translate('Hosts')
         );
-        $table = new HostsInFolderTable($this->db());
-        $table->filterParentIds([$parent]);
-        // $table->filterParentIds($ids);
-        $path = Html::tag('span', ['class' => 'dc-path'])->setSeparator(' > ');
-        foreach ($lookup->getObjectNames($lookup->listPathTo($parent)) as $id => $name) {
-            $path->add(Link::create(
-                $name,
-                'vspheredb/overview/hosts',
-                ['id' => $id]
-            ));
-        }
-        $this->content()->add([
-            $path,
-            sprintf(': %d Hosts', count($table))
-        ]);
+        $total = count($table);
         $table->renderTo($this);
+        $found = count($table);
+        if ($total === $found) {
+            $this->content()->prepend(sprintf('%d Hosts in ', $total));
+        } else {
+            $this->content()->prepend(sprintf('%d out of %d Hosts in ', $found, $total));
+        }
     }
 
     public function vmsAction()
     {
-        $parent = $this->params->getRequired('id');
-        $lookup = new PathLookup($this->db());
-        $name = $lookup->getObjectName($parent);
-        $ids = $lookup->listFoldersBelongingTo($parent);
-        $this
-            ->addSingleTab($this->translate('VMs'))
-            ->addTitle($name);
-        $this->actions()->add(
-            Link::create(
-                $this->translate('back'),
-                'vspheredb/overview',
-                null,
-                [
-                    'data-base-target' => '_main',
-                    'class' => 'icon-left-big'
-                ]
-            )
+        $this->addSingleTab($this->translate('VMs'));
+        $this->linkBackToOverview('vm');
+        $table = $this->eventuallyFilterByParent(
+            new VmsInFolderTable($this->db()),
+            'vspheredb/overview/vms',
+            $this->translate('Virtual Machines')
         );
-        $table = new VmsInFolderTable($this->db());
-        // $table->filterParentIds($ids);
-        $table->filterParentIds([$parent]);
-        $path = Html::tag('span', ['class' => 'dc-path'])->setSeparator(' > ');
-        foreach ($lookup->getObjectNames($lookup->listPathTo($parent)) as $id => $name) {
-            $path->add(Link::create(
-                $name,
-                'vspheredb/overview/vms',
-                ['id' => $id]
-            ));
-        }
-        $this->content()->add([
-            $path,
-            sprintf(': %d Virtual Machines', count($table))
-        ]);
+        $this->content()->add(sprintf(': %d Virtual Machines', count($table)));
         $table->renderTo($this);
+    }
+
+    public function allvmsAction()
+    {
+        $this->activateTab('allvms')
+            ->addTitle('Virtual Machines');
+
+        $table = new VmsTable($this->db());
+        $total = count($table);
+        $table->renderTo($this);
+        $found = count($table);
+
+        if ($total === $found) {
+            $this->content()->prepend(sprintf('%d Virtual Machines', $total));
+        } else {
+            $this->content()->prepend(sprintf('%d out of %d Virtual Machines', $found, $total));
+        }
     }
 
     public function datastoresAction()
     {
-        $parent = $this->params->getRequired('id');
-        $lookup = new PathLookup($this->db());
-        $name = $lookup->getObjectName($parent);
-        $ids = $lookup->listFoldersBelongingTo($parent);
-        $this
-            ->addSingleTab($this->translate('Datatores'))
-            ->addTitle($name);
+        $this->addSingleTab($this->translate('Datatores'));
+        $this->linkBackToOverview('datastore');
+        $table = $this->eventuallyFilterByParent(
+            new DatastoreTable($this->db()),
+            'vspheredb/overview/datastores',
+            $this->translate('Datastores')
+        );
+        $this->content()->add(sprintf(': %d Datastores', count($table)));
+        $table->renderTo($this);
+    }
+
+    protected function linkBackToOverview($type)
+    {
         $this->actions()->add(
             Link::create(
                 $this->translate('back'),
                 'vspheredb/overview',
-                null,
+                ['type' => $type],
                 [
                     'data-base-target' => '_main',
                     'class' => 'icon-left-big'
                 ]
             )
         );
-        $table = new DatastoreInFolderTable($this->db());
-        $table->filterParentIds([$parent]);
-        // $table->filterParentIds($ids);
+
+        return $this;
+    }
+
+    protected function eventuallyFilterByParent(ObjectsTable $table, $url, $defaultTitle)
+    {
+        $parent = $this->params->get('id');
+
+        if ($parent) {
+            $lookup = $this->pathLookup();
+            $name = $lookup->getObjectName($parent);
+            $ids = $lookup->listFoldersBelongingTo($parent);
+            $this->addTitle($name);
+            if ($this->params->get('showDescendants')) {
+                $table->filterParentIds($ids);
+            } else {
+                $table->filterParentIds([$parent]);
+            }
+            $this->addPathTo($parent, $url);
+        } else {
+            $this->addTitle($defaultTitle);
+        }
+
+        return $table;
+    }
+
+    protected function addPathTo($parent, $url)
+    {
+        $lookup = $this->pathLookup();
         $path = Html::tag('span', ['class' => 'dc-path'])->setSeparator(' > ');
         foreach ($lookup->getObjectNames($lookup->listPathTo($parent)) as $id => $name) {
             $path->add(Link::create(
                 $name,
-                'vspheredb/overview/datastores',
-                ['id' => $id]
+                $url,
+                [
+                    'id'              => $id,
+                    'showDescendants' => true,
+                ]
             ));
         }
-        $this->content()->add([
-            $path,
-            sprintf(': %d Datastores', count($table))
-        ]);
-        $table->renderTo($this);
+
+        $this->content()->add($path);
+    }
+
+    protected function activateTab($name)
+    {
+        $this->controls()->getTabs()->add('datastore', [
+            'label' => $this->translate('Datastores'),
+            'url'   => 'vspheredb/overview?type=datastore'
+        ])->add('host', [
+            'label' => $this->translate('Hosts'),
+            'url'   => 'vspheredb/overview?type=host'
+        ])->add('vm', [
+            'label' => $this->translate('VMs'),
+            'url'   => 'vspheredb/overview?type=vm'
+        ])->add('allvms', [
+            'label' => $this->translate('All VMs'),
+            'url'   => 'vspheredb/overview/allvms'
+        ])->activate($name);
+
+        return $this;
     }
 }
