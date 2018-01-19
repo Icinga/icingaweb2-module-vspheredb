ae8f6841dcd94c95460ccd6368ffe6a7a9ac4760
Thomas Gelf <thomas@gelf.net>
2018-01-19 11:52:06 +0100

    HealthCommand: remove superfluous code

--- application/clicommands/HealthCommand.php
+++ application/clicommands/HealthCommand.php
@@ -13,25 +13,16 @@ class HealthCommand extends CommandBase
 {
     public function checkAction()
     {
-        Benchmark::measure('Preparing the API');
         $api = $this->api();
-        Benchmark::measure('Logged in, ready to fetch');
-
-        try {
-            $time = $api->getCurrentTime()->format('U.u');
-            $timediff = microtime(true) - (float)$time;
-            if (abs($timediff) > 0.1) {
-                printf("%0.3fms Time difference detected\n", $timediff * 1000);
-            }
-
-            $vcenter = $this->getVCenter($api);
-            printf("Connected to a %s\n", $vcenter->getFullName());
-            echo $api->getVersionString();
-        } catch (\Exception $e) {
-            echo $e->getMessage() . "\n";
-            echo $e->getTraceAsString();
-            echo Benchmark::dump();
+        $time = $api->getCurrentTime()->format('U.u');
+        $timediff = microtime(true) - (float)$time;
+        if (abs($timediff) > 0.1) {
+            printf("%0.3fms Time difference detected\n", $timediff * 1000);
         }
+
+        $vcenter = $this->getVCenter($api);
+        printf("Connected to a %s\n", $vcenter->getFullName());
+        echo $api->getVersionString();
     }
 
     // Hint: this also updates the vCenter
