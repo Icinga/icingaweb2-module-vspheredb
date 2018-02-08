f8d5c25c29b3e52df017d18e1a3ab317a1ec0f5b
Thomas Gelf <thomas@gelf.net>
2018-02-08 16:41:17 +0100

    SyncCommand: add hardware sync, based on VCenter

--- application/clicommands/SyncCommand.php
+++ application/clicommands/SyncCommand.php
@@ -2,9 +2,11 @@
 
 namespace Icinga\Module\Vspheredb\Clicommands;
 
-use Icinga\Module\Vspheredb\Sync\Sync;
+use Icinga\Module\Vspheredb\Sync\SyncAllObjects;
 use Icinga\Module\Vspheredb\Sync\SyncPerfCounterInfo;
 use Icinga\Module\Vspheredb\Sync\SyncPerfCounters;
+use Icinga\Module\Vspheredb\Sync\SyncVmDatastoreUsage;
+use Icinga\Module\Vspheredb\Sync\SyncVmHardware;
 
 /**
  * Sync a vCenter or ESXi host
@@ -18,23 +20,35 @@ class SyncCommand extends CommandBase
      *
      * USAGE
      *
-     * icingacli vsphere sync objects --server_id <id>
+     * icingacli vsphere sync objects --vCenter <id>
      */
     public function objectsAction()
     {
-        $sync = new Sync($this->api(), $this->db());
-        $sync->syncAllObjects();
+        $sync = new SyncAllObjects($this->getVCenter());
+        $sync->run();
     }
 
     public function perfcountersAction()
     {
-        $sync = new SyncPerfCounters($this->api(), $this->db());
+        $sync = new SyncPerfCounters($this->getVCenter());
         $sync->run();
     }
 
     public function perfcounterinfoAction()
     {
-        $sync = new SyncPerfCounterInfo($this->api(), $this->db());
+        $sync = new SyncPerfCounterInfo($this->getVCenter());
+        $sync->run();
+    }
+
+    public function vmhardwareAction()
+    {
+        $sync = new SyncVmHardware($this->getVCenter());
+        $sync->run();
+    }
+
+    public function vmdatastoreusageAction()
+    {
+        $sync = new SyncVmDatastoreUsage($this->getVCenter());
         $sync->run();
     }
 }
