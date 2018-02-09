f7383fdc449c936fd2bf4e2ce74de8f420b7a417
Thomas Gelf <thomas@gelf.net>
2018-02-09 14:42:39 +0100

    HostSystem: cleanup, fetch service tag

--- library/Vspheredb/DbObject/HostSystem.php
+++ library/Vspheredb/DbObject/HostSystem.php
@@ -2,12 +2,18 @@
 
 namespace Icinga\Module\Vspheredb\DbObject;
 
+use DateTime;
+
 class HostSystem extends BaseDbObject
 {
+    protected $keyName = 'uuid';
+
     protected $table = 'host_system';
 
     protected $defaultProperties = [
-        'id'                      => null,
+        'uuid'                    => null,
+        'vcenter_uuid'            => null,
+        'host_name'               => null,
         'product_api_version'     => null,
         'product_full_name'       => null,
         'bios_version'            => null,
@@ -15,6 +21,7 @@ class HostSystem extends BaseDbObject
         'sysinfo_vendor'          => null,
         'sysinfo_model'           => null,
         'sysinfo_uuid'            => null,
+        'service_tag'             => null,
         'hardware_cpu_model'      => null,
         'hardware_cpu_mhz'        => null,
         'hardware_cpu_packages'   => null,
@@ -27,10 +34,11 @@ class HostSystem extends BaseDbObject
     ];
 
     protected $propertyMap = [
+        // config.fileSystemVolume.mountInfo
+        'name'                            => 'host_name',
         'config.product.apiVersion'       => 'product_api_version',
         'config.product.fullName'         => 'product_full_name',
         'hardware.biosInfo.biosVersion'   => 'bios_version',
-        // 'hardware.biosInfo.releaseDate'  => 'bios_release_date',
         'hardware.cpuInfo.numCpuPackages' => 'hardware_cpu_packages',
         'hardware.cpuInfo.numCpuCores'    => 'hardware_cpu_cores',
         'hardware.cpuInfo.numCpuThreads'  => 'hardware_cpu_threads',
@@ -40,7 +48,6 @@ class HostSystem extends BaseDbObject
         'runtime.powerState'              => 'runtime_power_state',
         'summary.hardware.cpuMhz'         => 'hardware_cpu_mhz',
         'summary.hardware.cpuModel'       => 'hardware_cpu_model',
-        // 'summary.hardware.memorySize'    => 'hardware_memory_size_mb', // div 1024?
         'summary.hardware.numHBAs'        => 'hardware_num_hba',
         'summary.hardware.numNics'        => 'hardware_num_nic',
     ];
@@ -49,7 +56,11 @@ class HostSystem extends BaseDbObject
     {
         return array_merge(
             parent::getDefaultPropertySet(),
-            ['summary.hardware.memorySize']
+            [
+                'summary.hardware.memorySize',
+                'hardware.biosInfo.releaseDate',
+                'summary.hardware.otherIdentifyingInfo',
+            ]
         );
     }
 
@@ -59,17 +70,41 @@ class HostSystem extends BaseDbObject
         return $db->fetchOne(
             $db->select()
                 ->from('virtual_machine', 'COUNT(*)')
-                ->where('runtime_host_id = ?', $this->get('id'))
+                ->where('runtime_host_uuid = ?', $this->get('uuid'))
         );
     }
 
-    public function setMapped($properties)
+    public function setMapped($properties, VCenter $vCenter)
     {
+        $this->setOtherIdentifyingInfo(
+            $properties->{'summary.hardware.otherIdentifyingInfo'}->HostSystemIdentificationInfo
+        );
+        $this->set(
+            'bios_release_date',
+            $this->formatBiosReleaseDate($properties->{'hardware.biosInfo.releaseDate'})
+        );
         $this->set(
             'hardware_memory_size_mb',
             floor($properties->{'summary.hardware.memorySize'} / (1024 * 1024))
         );
 
-        return parent::setMapped($properties);
+        return parent::setMapped($properties, $vCenter);
+    }
+
+    protected function setOtherIdentifyingInfo($infos)
+    {
+        foreach ($infos as $info) {
+            if ($info->identifierType->key === 'ServiceTag') {
+                $this->set(
+                    'service_tag',
+                    $info->identifierValue
+                );
+            }
+        }
+    }
+
+    protected function formatBiosReleaseDate($date)
+    {
+        return (new DateTime($date))->format('Y-m-d H:i:s');
     }
 }
