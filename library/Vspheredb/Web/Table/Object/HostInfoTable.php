e87fb85498606adaae57501f4f80a7b9efcb25c7
Thomas Gelf <thomas@gelf.net>
2018-02-09 14:41:14 +0100

    HostInfoTable: show (and link) service Tag and...
    ...more formatting

--- library/Vspheredb/Web/Table/Object/HostInfoTable.php
+++ library/Vspheredb/Web/Table/Object/HostInfoTable.php
@@ -33,15 +33,15 @@ class HostInfoTable extends NameValueTable
     protected function assemble()
     {
         $host = $this->host;
-        $id = $host->get('id');
+        $uuid = $host->get('uuid');
         $lookup = $this->pathLookup;
 
         $path = Html::tag('span', ['class' => 'dc-path'])->setSeparator(' > ');
-        foreach ($lookup->getObjectNames($lookup->listPathTo($id, false)) as $parentId => $name) {
+        foreach ($lookup->getObjectNames($lookup->listPathTo($uuid, false)) as $parentUuid => $name) {
             $path->add(Link::create(
                 $name,
                 'vspheredb/hosts',
-                ['id' => $parentId],
+                ['uuid' => bin2hex($parentUuid)],
                 ['data-base-target' => '_main']
             ));
         }
@@ -57,6 +57,7 @@ class HostInfoTable extends NameValueTable
             // $this->translate('BIOS Release Date') => $vm->get('bios_release_date'),
             $this->translate('Vendor')       => $host->get('sysinfo_vendor'),
             $this->translate('Model')        => $host->get('sysinfo_model'),
+            $this->translate('Service Tag')  => $this->getFormattedServiceTag(),
             $this->translate('CPU Model')    => $host->get('hardware_cpu_model'),
             $this->translate('CPU Packages') => $host->get('hardware_cpu_packages'),
             $this->translate('CPU Cores')    => $host->get('hardware_cpu_cores'),
@@ -66,18 +67,42 @@ class HostInfoTable extends NameValueTable
             $this->translate('Vms')          => Link::create(
                 $host->countVms(),
                 'vspheredb/host/vms',
-                ['id' => $id]
+                ['uuid' => bin2hex($uuid)]
             ),
         ]);
+    }
+
+    protected function getFormattedServiceTag()
+    {
+        $host = $this->host;
+        $tag = $host->get('service_tag');
+        if ($host->get('sysinfo_vendor') === 'Dell Inc.') {
+            $url = sprintf(
+                'http://www.dell.com/support/home/de/de/debsdt1/product-support/servicetag/%s/drivers',
+                strtolower($tag)
+            );
+
+            return Html::tag(
+                'a',
+                [
+                    'href'   => $url,
+                    'target' => '_blank',
+                    'title'  => $this->translate('Dell Support Page')
+                ],
+                $tag
+            );
+        } else {
+            return $tag;
+        }
     }
 
     protected function getFormattedMemory()
     {
         return number_format(
-                $this->host->get('hardware_memory_size_mb'),
-                0,
-                ',',
-                '.'
-            ) . ' MB';
+            $this->host->get('hardware_memory_size_mb'),
+            0,
+            ',',
+            '.'
+        ) . ' MB';
     }
 }
