f77f933ecbf9fd31e1bf23cd4f7841d14912faff
Thomas Gelf <thomas@gelf.net>
2018-02-09 15:42:13 +0100

    HealthCommand: remove obsolete code

--- application/clicommands/HealthCommand.php
+++ application/clicommands/HealthCommand.php
@@ -2,10 +2,6 @@
 
 namespace Icinga\Module\Vspheredb\Clicommands;
 
-use Icinga\Application\Benchmark;
-use Icinga\Module\Vspheredb\Api;
-use Icinga\Module\Vspheredb\DbObject\VCenter;
-
 /**
  * Health information for a vCenter or ESXi host
  */
@@ -13,41 +9,15 @@ class HealthCommand extends CommandBase
 {
     public function checkAction()
     {
-        $api = $this->api();
+        $vCenter = $this->getVCenter();
+        $api = $vCenter->getApi();
         $time = $api->getCurrentTime()->format('U.u');
-        $timediff = microtime(true) - (float)$time;
-        if (abs($timediff) > 0.1) {
-            printf("%0.3fms Time difference detected\n", $timediff * 1000);
+        $timeDiff = microtime(true) - (float)$time;
+        if (abs($timeDiff) > 0.1) {
+            printf("%0.3fms Time difference detected\n", $timeDiff * 1000);
         }
 
-        $vcenter = $this->getVCenter($api);
-        printf("Connected to a %s\n", $vcenter->getFullName());
+        printf("Connected to a %s\n", $vCenter->getFullName());
         echo $api->getVersionString();
-    }
-
-    // Hint: this also updates the vCenter
-    protected function getVCenter(Api $api)
-    {
-        $about = $api->getAbout();
-        $uuid = $api->getBinaryUuid();
-        if (VCenter::exists($uuid, $this->db())) {
-            $vcenter = VCenter::load($uuid, $this->db());
-        } else {
-            $vcenter = VCenter::create([], $this->db());
-        }
-        $vcenter->setMapped($about);
-
-        if ($vcenter->hasBeenModified()) {
-            if ($vcenter->hasBeenLoadedFromDb()) {
-                $msg = 'vCenter has been modified';
-            } else {
-                $msg = 'vCenter has been created';
-            }
-
-            $vcenter->store();
-            echo "$msg\n";
-        }
-
-        return $vcenter;
     }
 }
