41ad1bd119e1ec8d9973c37739fad70994c86679
Thomas Gelf <thomas@gelf.net>
2018-02-23 15:41:30 +0100

    HostBiosTable: this is now obsolete

--- application/controllers/HostsController.php
+++ application/controllers/HostsController.php
@@ -5,7 +5,6 @@ namespace Icinga\Module\Vspheredb\Controllers;
 use Icinga\Authentication\Auth;
 use Icinga\Module\Vspheredb\Web\Widget\AdditionalTableActions;
 use Icinga\Module\Vspheredb\Web\Controller\ObjectsController;
-use Icinga\Module\Vspheredb\Web\Table\Objects\HostsBiosTable;
 use Icinga\Module\Vspheredb\Web\Table\Objects\HostsTable;
 
 class HostsController extends ObjectsController
@@ -20,15 +19,5 @@ class HostsController extends ObjectsController
         $table->handleSortUrl($this->url());
 
         $this->showTable($table, 'vspheredb/hosts', $this->translate('Hosts'));
-    }
-
-    public function spectreAction()
-    {
-        $this->addSingleTab($this->translate('Hosts'));
-        $this->showTable(
-            new HostsBiosTable($this->db()),
-            'vspheredb/hosts/spectre',
-            $this->translate('Hosts with Spectre/Meltdown hints')
-        );
     }
 }
