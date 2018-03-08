addf31da6bbc756a771fbd042b9f77dd9142ffd9
Thomas Gelf <thomas@gelf.net>
2018-03-08 18:09:35 +0100

    VCenterServersTable: just a new table

---
+++ library/Vspheredb/Web/Table/Objects/VCenterServersTable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Web\Table\Objects;
+
+use dipl\Html\Link;
+use Icinga\Module\Vspheredb\Web\Table\BaseTable;
+use Icinga\Module\Vspheredb\Web\Table\SimpleColumn;
+
+class VCenterServersTable extends BaseTable
+{
+    protected function initialize()
+    {
+        $this->addAvailableColumns([
+            (new SimpleColumn('server', $this->translate('Server'), [
+                'scheme' => 'vcs.scheme',
+                'username' => 'vcs.username',
+                'host' => 'vcs.host',
+                'id' => 'vcs.id',
+            ]))->setRenderer(function ($row) {
+                return Link::create(
+                    $this->makeUrl($row),
+                    'vspheredb/vcenter/server',
+                    ['id' => $row->id]
+                );
+            })->setDefaultSortDirection('DESC'),
+            (new SimpleColumn('vcenter', $this->translate('VCenter'), 'vc.name')),
+        ]);
+    }
+
+    protected function makeUrl($row)
+    {
+        return sprintf(
+            '%s://%s@%s',
+            $row->scheme,
+            $row->username,
+            $row->host
+        );
+    }
+
+    public function prepareQuery()
+    {
+        return $this->db()->select()->from(
+            ['vcs' => 'vcenter_server'],
+            $this->getRequiredDbColumns()
+        )->joinLeft(
+            ['vc' => 'vcenter'],
+            // 'vc.instance_uuid = vcs.vcenter_uuid',
+            'vc.id = vcs.vcenter_id',
+            []
+        );
+    }
+}
