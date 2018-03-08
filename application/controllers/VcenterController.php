57de096d73c0fe6326e0cd98f244d7ffc7d9c16d
Thomas Gelf <thomas@gelf.net>
2018-03-08 18:25:05 +0100

    VCenterServerForm: allow to configure Connections

--- application/controllers/VcenterController.php
+++ application/controllers/VcenterController.php
@@ -2,20 +2,74 @@
 
 namespace Icinga\Module\Vspheredb\Controllers;
 
+use dipl\Html\Html;
+use dipl\Html\Link;
+use Icinga\Module\Vspheredb\Db;
 use Icinga\Module\Vspheredb\DbObject\VCenter;
 use Icinga\Module\Vspheredb\Web\Controller;
+use Icinga\Module\Vspheredb\Web\Form\VCenterServerForm;
 use Icinga\Module\Vspheredb\Web\Table\Object\VCenterInfoTable;
+use Icinga\Module\Vspheredb\Web\Table\Objects\VCenterServersTable;
 use Icinga\Module\Vspheredb\Web\Widget\VCenterSummaries;
 
 class VcenterController extends Controller
 {
     public function indexAction()
     {
-        $this->addSingleTab($this->translate('vCenter Overview'));
+        $this->handleTabs();
         $vcenter = VCenter::loadWithAutoIncId(1, $this->db());
         $this->content()->add([
             new VCenterInfoTable($vcenter),
             new VCenterSummaries($vcenter),
         ]);
+    }
+
+    public function serversAction()
+    {
+        $this->handleTabs();
+        $this->addTitle($this->translate('vCenter Servers'));
+        $this->actions()->add(
+            Link::create(
+                $this->translate('Add'),
+                'vspheredb/vcenter/server',
+                null,
+                [
+                    'class' => 'icon-plus',
+                    'data-base-target' => '_next'
+                ]
+            )
+        );
+
+        $table = new VCenterServersTable($this->db());
+        $table->renderTo($this);
+    }
+
+    public function serverAction()
+    {
+        $this->addSingleTab($this->translate('vCenter Server'));
+
+        $form = new VCenterServerForm();
+        $form->setVsphereDb(Db::newConfiguredInstance());
+        if ($id = $this->params->get('id')) {
+            $form->loadObject($id);
+            $this->addTitle($form->getObject()->get('host'));
+        }
+        $form->handleRequest();
+        $this->content()->add(Html::tag(
+            'div',
+            ['class' => 'icinga-module module-director'],
+            $form
+        ));
+    }
+
+    protected function handleTabs()
+    {
+        $this->tabs()->add('index', [
+            'label'     => $this->translate('vCenter Overview'),
+            'url'       => 'vspheredb/vcenter',
+        ])->add('servers', [
+            'label'     => $this->translate('Servers'),
+            'url'       => 'vspheredb/vcenter/servers',
+        ])->activate($this->getRequest()->getActionName());
     }
 }
