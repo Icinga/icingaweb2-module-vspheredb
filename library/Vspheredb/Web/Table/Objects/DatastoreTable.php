e1f0d89b270d9c7d9c5c1edb2caec7f12509d3cb
Thomas Gelf <thomas@gelf.net>
2018-03-11 14:38:32 +0100

    DatastoreTable: lots of cleanup, performance tuning

--- library/Vspheredb/Web/Table/Objects/DatastoreTable.php
+++ library/Vspheredb/Web/Table/Objects/DatastoreTable.php
@@ -5,7 +5,6 @@ namespace Icinga\Module\Vspheredb\Web\Table\Objects;
 use dipl\Html\Icon;
 use Icinga\Module\Vspheredb\Db;
 use Icinga\Module\Vspheredb\DbObject\Datastore;
-use Icinga\Module\Vspheredb\Web\Table\SimpleColumn;
 use Icinga\Module\Vspheredb\Web\Widget\DatastoreUsage;
 use Icinga\Util\Format;
 use dipl\Html\Link;
@@ -15,23 +14,22 @@ class DatastoreTable extends ObjectsTable
     protected function initialize()
     {
         $this->addAvailableColumns([
-            (new SimpleColumn('overall_status', $this->translate('Status'), 'o.overall_status'))
+            $this->createColumn('overall_status', $this->translate('Status'), 'o.overall_status')
                 ->setRenderer(function ($row) {
                     return Icon::create('ok', [
                         'title' => $this->getStatusDescription($row->overall_status),
                         'class' => [ 'state', $row->overall_status ]
                     ]);
                 })->setDefaultSortDirection('DESC'),
-            (new SimpleColumn('object_name', $this->translate('Name'), [
+            $this->createColumn('object_name', $this->translate('Name'), [
                 'object_name'          => 'o.object_name',
                 'uuid'                 => 'o.uuid',
-//                'cnt_vm'               => 'COUNT(*)',
                 'capacity'             => 'ds.capacity',
                 'free_space'           => 'ds.free_space',
                 'uncommitted'          => 'ds.uncommitted',
                 'free_space_percent'   => '(ds.free_space / ds.capacity) * 100',
                 'uncommitted_percent'  => '(ds.uncommitted / ds.capacity) * 100',
-            ]))->setRenderer(function ($row) {
+            ])->setRenderer(function ($row) {
                 $title = sprintf(
                     // '%d VM(s), %s of %s used, %s uncommitted',
                     '%s of %s used, %s uncommitted',
@@ -40,6 +38,7 @@ class DatastoreTable extends ObjectsTable
                     Format::bytes($row->capacity, Format::STANDARD_IEC),
                     $this->formatBytesPercent($row, 'uncommitted')
                 );
+
                 return Link::create(
                     $row->object_name,
                     'vspheredb/datastore',
@@ -47,36 +46,43 @@ class DatastoreTable extends ObjectsTable
                     ['title' => $title]
                 );
             }),
-            (new SimpleColumn('free_space', $this->translate('Free'), 'ds.free_space'))->setRenderer(function ($row) {
-                return Format::bytes($row->free_space, Format::STANDARD_IEC);
-            }),
-            (new SimpleColumn('free_space_percent', $this->translate('Free (%)'), [
+            $this->createColumn('free_space', $this->translate('Free'), 'ds.free_space')
+                ->setRenderer(function ($row) {
+                    return Format::bytes($row->free_space, Format::STANDARD_IEC);
+                }),
+            $this->createColumn('cnt_vms', $this->translate('VMs'), [
+                'cnt_vms' => 'COALESCE(vdu.cnt_vms, 0)',
+            ])->setDefaultSortDirection('DESC'),
+            $this->createColumn('free_space_percent', $this->translate('Free (%)'), [
                 'free_space_percent'  => '(ds.free_space / ds.capacity) * 100'
-            ]))->setRenderer(function ($row) {
+            ])->setRenderer(function ($row) {
                 return $this->formatPercent($row->free_space_percent);
             }),
-            (new SimpleColumn('uncommitted', $this->translate('Uncommitted'), 'ds.uncommitted'))->setRenderer(function ($row) {
-                return Format::bytes($row->uncommitted, Format::STANDARD_IEC);
-            }),
-            (new SimpleColumn('uncommitted_percent', $this->translate('Uncommitted (%)'), [
+            $this->createColumn('uncommitted', $this->translate('Uncommitted'), 'ds.uncommitted')
+                ->setRenderer(function ($row) {
+                    return Format::bytes($row->uncommitted, Format::STANDARD_IEC);
+                }),
+            $this->createColumn('uncommitted_percent', $this->translate('Uncommitted (%)'), [
                 'uncommitted_percent'  => '(ds.uncommitted / ds.capacity) * 100'
-            ]))->setRenderer(function ($row) {
+            ])->setRenderer(function ($row) {
                 return $this->formatPercent($row->free_space_percent);
             }),
-            (new SimpleColumn('size', $this->translate('Size'), 'ds.capacity'))->setRenderer(function ($row) {
-                return Format::bytes($row->capacity, Format::STANDARD_IEC);
-            }),
-            (new SimpleColumn('usage', $this->translate('Usage'), [
+            $this->createColumn('size', $this->translate('Size'), 'ds.capacity')
+                ->setRenderer(function ($row) {
+                    return Format::bytes($row->capacity, Format::STANDARD_IEC);
+                }),
+            $this->createColumn('usage', $this->translate('Usage'), [
                 'uuid' => 'uuid'
-            ]))->setRenderer(function ($row) {
-                /** @var Db $connection */
-                $connection = $this->connection();
-                $usage = new DatastoreUsage(Datastore::load($row->uuid, $connection));
-                $usage->attributes()->add('class', 'compact');
-                $usage->loadAllVmDisks()->addFreeDatastoreSpace();
+            ])->setSortExpression('1 - (ds.free_space / ds.capacity) ')
+                ->setRenderer(function ($row) {
+                    /** @var Db $connection */
+                    $connection = $this->connection();
+                    $usage = new DatastoreUsage(Datastore::load($row->uuid, $connection));
+                    $usage->attributes()->add('class', 'compact');
+                    $usage->loadAllVmDisks()->addFreeDatastoreSpace();
 
-                return $usage;
-            }),
+                    return $usage;
+                }),
         ]);
     }
 
@@ -85,9 +91,7 @@ class DatastoreTable extends ObjectsTable
         return [
             'overall_status',
             'object_name',
-            'runtime_power_state',
-            'cpu_usage',
-            'memory_usage',
+            'usage',
         ];
     }
 
@@ -119,6 +123,16 @@ class DatastoreTable extends ObjectsTable
         return $descriptions[$status];
     }
 
+    public function sortBy($columns)
+    {
+        parent::sortBy($columns);
+
+        $this->getQuery()->order('object_name');
+
+        return $this;
+    }
+
+
     public function prepareQuery()
     {
         $query = $this->db()->select()->from(
@@ -129,6 +143,18 @@ class DatastoreTable extends ObjectsTable
             'o.uuid = ds.uuid',
             []
         )->group('o.uuid');
+
+        if ($this->hasColumn('cnt_vms')) {
+            $vduQuery = $this->db()->select()->from('vm_datastore_usage', [
+                'cnt_vms' => 'COUNT(*)',
+                'ds_uuid' => 'datastore_uuid',
+            ])->group('datastore_uuid');
+            $query->joinLeft(
+                ['vdu' => $vduQuery],
+                'vdu.ds_uuid = o.uuid',
+                []
+            );
+        }
 
         if ($this->parentUuids) {
             $query->where('o.parent_uuid IN (?)', $this->parentUuids);
