2f1eb26ad03db3cffd4ee14200632a71092d3af0
Thomas Gelf <thomas@gelf.net>
2018-03-20 14:48:49 +0100

    Datacenters: controller and table

---
+++ application/controllers/DatacentersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Controllers;
+
+use Icinga\Authentication\Auth;
+use Icinga\Module\Vspheredb\Web\Controller\ObjectsController;
+use Icinga\Module\Vspheredb\Web\Table\Objects\DatacentersTable;
+use Icinga\Module\Vspheredb\Web\Widget\AdditionalTableActions;
+use Icinga\Module\Vspheredb\Web\Widget\Summaries;
+
+class DatacentersController extends ObjectsController
+{
+    public function indexAction()
+    {
+        $this->addSingleTab($this->translate('Datacenters'));
+        $table = new DatacentersTable($this->db());
+        (new AdditionalTableActions($table, Auth::getInstance(), $this->url()))
+            ->appendTo($this->actions());
+        $table->handleSortUrl($this->url());
+        $this->showTable($table, 'vspheredb/datacenters', $this->translate('Datacenters'));
+        $summaries = new Summaries($table, $this->db(), $this->url());
+        $this->content()->prepend($summaries);
+    }
+}
---
+++ library/Vspheredb/Web/Table/Objects/DatacentersTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Web\Table\Objects;
+
+class DatacentersTable extends ObjectsTable
+{
+    protected $baseUrl = 'vspheredb/datacenter';
+
+    protected function initialize()
+    {
+        $this->addAvailableColumns([
+            $this->createOverallStatusColumn(),
+            $this->createObjectNameColumn()
+
+        ]);
+    }
+
+    public function getDefaultColumnNames()
+    {
+        return [
+            'overall_status',
+            'object_name',
+        ];
+    }
+
+    public function prepareQuery()
+    {
+        $query = $this->db()->select()->from(
+            ['o' => 'object'],
+            $this->getRequiredDbColumns()
+        )->where('object_type = ?', 'DataCenter');
+
+        if ($this->parentUuids) {
+            $query->where('o.parent_uuid IN (?)', $this->parentUuids);
+        }
+
+        return $query;
+    }
+}
