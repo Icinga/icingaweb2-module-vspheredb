c235ab5c24a96b9d4f0c3d5f253b9353c52a5e93
Thomas Gelf <thomas@gelf.net>
2018-03-22 18:17:56 +0100

    SyncHelper: attempting to move logic to a trait

---
+++ library/Vspheredb/Sync/SyncHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Sync;
+
+use Icinga\Application\Logger;
+use Icinga\Module\Vspheredb\DbObject\VCenter;
+
+trait SyncHelper
+{
+    /** @var VCenter */
+    protected $vCenter;
+
+    public function __construct(VCenter $vCenter)
+    {
+        $this->vCenter = $vCenter;
+    }
+
+    /**
+     * @param \Zend_Db_Adapter_Abstract $db
+     * @param \Icinga\Module\Vspheredb\DbObject\BaseDbObject[] $objects
+     * @param $seen
+     */
+    protected function storeObjects(\Zend_Db_Adapter_Abstract $db, array $objects, array $seen)
+    {
+        $insert = 0;
+        $update = 0;
+        $delete = 0;
+        $db->beginTransaction();
+        foreach ($objects as $key => $object) {
+            if (! array_key_exists($key, $seen)) {
+                $object->delete();
+                $delete++;
+            } elseif ($object->hasBeenLoadedFromDb()) {
+                if ($object->hasBeenModified()) {
+                    $update++;
+                    $object->store();
+                }
+            } else {
+                $object->store();
+                $insert++;
+            }
+        }
+
+        $db->commit();
+        Logger::debug("$insert created, $update changed, $delete deleted");
+    }
+}
