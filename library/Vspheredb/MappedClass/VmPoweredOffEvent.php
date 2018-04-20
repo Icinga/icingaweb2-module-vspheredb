8b4ce5602884c16ef3fd2881f161a456ea36a84b
Thomas Gelf <thomas@gelf.net>
2018-04-20 14:58:52 +0200

    VmEvents: update related power info

--- library/Vspheredb/MappedClass/VmPoweredOffEvent.php
+++ library/Vspheredb/MappedClass/VmPoweredOffEvent.php
@@ -2,6 +2,22 @@
 
 namespace Icinga\Module\Vspheredb\MappedClass;
 
+use Icinga\Module\Vspheredb\DbObject\VCenter;
+use Zend_Db_Adapter_Abstract as ZfDbAdapter;
+
 class VmPoweredOffEvent extends VmEvent
 {
+    /**
+     * @param ZfDbAdapter $db
+     * @param VCenter $vCenter
+     * @throws \Zend_Db_Adapter_Exception
+     */
+    public function store(ZfDbAdapter $db, VCenter $vCenter)
+    {
+        parent::store($db, $vCenter);
+
+        $db->update('virtual_machine', [
+            'runtime_power_state' => 'poweredOff'
+        ], $db->quoteInto('uuid = ?', $vCenter->makeBinaryGlobalUuid($this->vm->vm->_)));
+    }
 }
--- library/Vspheredb/MappedClass/VmPoweredOnEvent.php
+++ library/Vspheredb/MappedClass/VmPoweredOnEvent.php
@@ -2,6 +2,22 @@
 
 namespace Icinga\Module\Vspheredb\MappedClass;
 
+use Icinga\Module\Vspheredb\DbObject\VCenter;
+use Zend_Db_Adapter_Abstract as ZfDbAdapter;
+
 class VmPoweredOnEvent extends VmEvent
 {
+    /**
+     * @param ZfDbAdapter $db
+     * @param VCenter $vCenter
+     * @throws \Zend_Db_Adapter_Exception
+     */
+    public function store(ZfDbAdapter $db, VCenter $vCenter)
+    {
+        parent::store($db, $vCenter);
+
+        $db->update('virtual_machine', [
+            'runtime_power_state' => 'poweredOn'
+        ], $db->quoteInto('uuid = ?', $vCenter->makeBinaryGlobalUuid($this->vm->vm->_)));
+    }
 }
