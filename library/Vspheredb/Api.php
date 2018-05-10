a3a7c000bcfcbfa803cc8c14c7040cb939f02f1e
Thomas Gelf <thomas@gelf.net>
2018-05-10 21:36:40 +0200

    ApiClassMap: extract code from Api class

--- library/Vspheredb/Api.php
+++ library/Vspheredb/Api.php
@@ -272,7 +272,7 @@ class Api
                 'location'           => $this->makeLocation(),
                 'exceptions'         => true,
                 'connection_timeout' => 10,
-                'classmap'           => $this->getClassMap(),
+                'classmap'           => ApiClassMap::getMap(),
                 'features'           => $features,
                 'cache_wsdl'         => WSDL_CACHE_NONE
             ];
@@ -283,78 +283,6 @@ class Api
         }
 
         return $this->soapClient;
-    }
-
-    protected function getClassMap()
-    {
-        $base = "Icinga\\Module\\Vspheredb\\MappedClass";
-
-        $map = [
-            //'ManagedObjectNotFoundFault' => "$base\\ManagedObjectNotFoundFault",
-            // 'AlarmEvent'              => "$base\\AlarmEvent",
-            'AlarmAcknowledgedEvent'  => "$base\\AlarmAcknowledgedEvent",
-            'AlarmClearedEvent'       => "$base\\AlarmClearedEvent",
-            'AlarmCreatedEvent'       => "$base\\AlarmCreatedEvent",
-            'AlarmReconfiguredEvent'  => "$base\\AlarmReconfiguredEvent",
-            'AlarmRemovedEvent'       => "$base\\AlarmRemovedEvent",
-            'AlarmStatusChangedEvent' => "$base\\AlarmStatusChangedEvent",
-
-            // AlarmActionTriggeredEvent
-            // AlarmEmailCompletedEvent
-            // AlarmEmailFailedEvent
-            // AlarmScriptCompleteEvent
-            // AlarmScriptFailedEvent
-            // AlarmSnmpCompletedEvent
-            // AlarmSnmpFailedEvent
-
-            'UserLoginSessionEvent'            => "$base\\UserLoginSessionEvent",
-            'SessionTerminatedEvent'           => "$base\\SessionTerminatedEvent",
-            'NoAccessUserEvent'                => "$base\\NoAccessUserEvent",
-            'BadUsernameSessionEvent'          => "$base\\BadUsernameSessionEvent",
-            'GlobalMessageChangedEvent'        => "$base\\GlobalMessageChangedEvent",
-            'UserLogoutSessionEvent'           => "$base\\UserLogoutSessionEvent",
-            'AlreadyAuthenticatedSessionEvent' => "$base\\AlreadyAuthenticatedSessionEvent",
-
-            // VmMessageEvent
-            // TaskEvent
-            // VmResettingEvent
-            // EventEx
-            // AlarmStatusChangedEvent
-            // UserLoginSessionEvent
-            // DrsRuleViolationEvent
-            // DrsSoftRuleViolationEvent
-            // NonVIWorkloadDetectedOnDatastoreEvent
-            // VmAcquiredTicketEvent
-            // CustomFieldValueChangedEvent
-            //
-
-            'VmFailedMigrateEvent'    => "$base\\VmFailedMigrateEvent",
-            'MigrationEvent'          => "$base\\MigrationEvent",
-            'VmBeingMigratedEvent'    => "$base\\VmBeingMigratedEvent",
-            'VmBeingHotMigratedEvent' => "$base\\VmBeingHotMigratedEvent",
-            'VmEmigratingEvent'       => "$base\\VmEmigratingEvent",
-            'VmMigratedEvent'         => "$base\\VmMigratedEvent",
-
-            'VmBeingCreatedEvent'  => "$base\\VmBeingCreatedEvent",
-            'VmCreatedEvent'       => "$base\\VmCreatedEvent",
-            'VmPoweredOnEvent'     => "$base\\VmPoweredOnEvent",
-            'VmPoweredOffEvent'    => "$base\\VmPoweredOffEvent",
-            'VmSuspendedEvent'     => "$base\\VmSuspendedEvent",
-            'VmReconfiguredEvent'  => "$base\\VmReconfiguredEvent",
-            'VmStartingEvent'      => "$base\\VmStartingEvent",
-            'VmStoppingEvent'      => "$base\\VmStoppingEvent",
-            // Not seen yet:
-            'VmBeingDeployedEvent' => "$base\\VmBeingDeployedEvent",
-        ];
-
-        $base = "Icinga\\Module\\Vspheredb\\VmwareDataType";
-
-        $map += [
-            'ManagedObjectReference' => "$base\\ManagedObjectReference",
-            'NumericRange'           => "$base\\NumericRange",
-        ];
-
-        return $map;
     }
 
     /**
---
+++ library/Vspheredb/ApiClassMap.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Icinga\Module\Vspheredb;
+
+class ApiClassMap
+{
+    public static function getMap()
+    {
+        $base = __NAMESPACE__ . "\\MappedClass";
+
+        $map = [
+            //'ManagedObjectNotFoundFault' => "$base\\ManagedObjectNotFoundFault",
+            // 'AlarmEvent'              => "$base\\AlarmEvent",
+            'AlarmAcknowledgedEvent'  => "$base\\AlarmAcknowledgedEvent",
+            'AlarmClearedEvent'       => "$base\\AlarmClearedEvent",
+            'AlarmCreatedEvent'       => "$base\\AlarmCreatedEvent",
+            'AlarmReconfiguredEvent'  => "$base\\AlarmReconfiguredEvent",
+            'AlarmRemovedEvent'       => "$base\\AlarmRemovedEvent",
+            'AlarmStatusChangedEvent' => "$base\\AlarmStatusChangedEvent",
+
+            // AlarmActionTriggeredEvent
+            // AlarmEmailCompletedEvent
+            // AlarmEmailFailedEvent
+            // AlarmScriptCompleteEvent
+            // AlarmScriptFailedEvent
+            // AlarmSnmpCompletedEvent
+            // AlarmSnmpFailedEvent
+
+            'UserLoginSessionEvent'            => "$base\\UserLoginSessionEvent",
+            'SessionTerminatedEvent'           => "$base\\SessionTerminatedEvent",
+            'NoAccessUserEvent'                => "$base\\NoAccessUserEvent",
+            'BadUsernameSessionEvent'          => "$base\\BadUsernameSessionEvent",
+            'GlobalMessageChangedEvent'        => "$base\\GlobalMessageChangedEvent",
+            'UserLogoutSessionEvent'           => "$base\\UserLogoutSessionEvent",
+            'AlreadyAuthenticatedSessionEvent' => "$base\\AlreadyAuthenticatedSessionEvent",
+
+            // VmMessageEvent
+            // TaskEvent
+            // VmResettingEvent
+            // EventEx
+            // AlarmStatusChangedEvent
+            // UserLoginSessionEvent
+            // DrsRuleViolationEvent
+            // DrsSoftRuleViolationEvent
+            // NonVIWorkloadDetectedOnDatastoreEvent
+            // VmAcquiredTicketEvent
+            // CustomFieldValueChangedEvent
+            //
+
+            'VmFailedMigrateEvent'    => "$base\\VmFailedMigrateEvent",
+            'MigrationEvent'          => "$base\\MigrationEvent",
+            'VmBeingMigratedEvent'    => "$base\\VmBeingMigratedEvent",
+            'VmBeingHotMigratedEvent' => "$base\\VmBeingHotMigratedEvent",
+            'VmEmigratingEvent'       => "$base\\VmEmigratingEvent",
+            'VmMigratedEvent'         => "$base\\VmMigratedEvent",
+
+            'VmBeingCreatedEvent'  => "$base\\VmBeingCreatedEvent",
+            'VmCreatedEvent'       => "$base\\VmCreatedEvent",
+            'VmPoweredOnEvent'     => "$base\\VmPoweredOnEvent",
+            'VmPoweredOffEvent'    => "$base\\VmPoweredOffEvent",
+            'VmSuspendedEvent'     => "$base\\VmSuspendedEvent",
+            'VmReconfiguredEvent'  => "$base\\VmReconfiguredEvent",
+            'VmStartingEvent'      => "$base\\VmStartingEvent",
+            'VmStoppingEvent'      => "$base\\VmStoppingEvent",
+            // Not seen yet:
+            'VmBeingDeployedEvent' => "$base\\VmBeingDeployedEvent",
+        ];
+
+        $base = __NAMESPACE__ . "\\VmwareDataType";
+
+        $map += [
+            'ManagedObjectReference' => "$base\\ManagedObjectReference",
+            'NumericRange'           => "$base\\NumericRange",
+        ];
+
+        return $map;
+    }
+}
