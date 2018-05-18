2524d86a5cc3c83c19a729423708bd92fe1068f0
Thomas Gelf <thomas@gelf.net>
2018-05-18 11:17:22 +0200

    IcingaHostStatusRenderer: new widget

---
+++ library/Vspheredb/Web/Widget/IcingaHostStatusRenderer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Web\Widget;
+
+use dipl\Html\Html;
+use dipl\Html\Icon;
+use dipl\Translation\TranslationHelper;
+
+class IcingaHostStatusRenderer extends Html
+{
+    use TranslationHelper;
+
+    public function __invoke($state)
+    {
+        if (is_object($state)) {
+            $state = $state->overall_status;
+        }
+
+        return Icon::create('eye', [
+            'title' => $this->getStatusDescription($state),
+            'class' => [ 'state', $state ]
+        ]);
+    }
+
+    protected function getStatusDescription($status)
+    {
+        $descriptions = [
+            'UP'   => $this->translate('This system is up'),
+            'DOWN'        => $this->translate('This system is down'),
+            'UNREACHABLE' => $this->translate('Unreachable - another device might be responsible for this outage'),
+            'PENDING'     => $this->translate('Pending - this host has never been checked'),
+        ];
+
+        return $descriptions[$status];
+    }
+}
