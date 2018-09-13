7f48bf035add2c75b6cc26252bad49d2c491bc63
Thomas Gelf <thomas@gelf.net>
2018-09-13 11:31:35 +0200

    KeepWorkerRunning: pass through arguments

--- library/Vspheredb/Daemon/Task/KeepWorkerRunning.php
+++ library/Vspheredb/Daemon/Task/KeepWorkerRunning.php
@@ -6,12 +6,15 @@ class KeepWorkerRunning extends KeepProcessRunning
 {
     protected $command;
 
-    public function __construct($action, $argv = null)
+    public function __construct($action, $argv = null, $arguments = [])
     {
         if ($argv === null) {
             global $argv;
         }
         $this->command = $argv[0];
         $this->arguments = ['vspheredb', 'daemon', $action];
+        foreach ($arguments as $arg) {
+            $this->arguments[] = $arg;
+        }
     }
 }
