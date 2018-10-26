7318f08a8c9c9e9c00e93d662afd4754508bff50
Thomas Gelf <thomas@gelf.net>
2018-10-26 11:15:58 +0200

    VmStartingEvent: a starting VM is poweredOn

--- library/Vspheredb/MappedClass/VmStartingEvent.php
+++ library/Vspheredb/MappedClass/VmStartingEvent.php
@@ -2,6 +2,23 @@
 
 namespace Icinga\Module\Vspheredb\MappedClass;
 
+use Icinga\Module\Vspheredb\DbObject\VCenter;
+use Zend_Db_Adapter_Abstract as ZfDbAdapter;
+
 class VmStartingEvent extends VmEvent
 {
+    /**
+     * @param ZfDbAdapter $db
+     * @param VCenter $vCenter
+     * @throws \Zend_Db_Adapter_Exception
+     */
+    public function store(ZfDbAdapter $db, VCenter $vCenter)
+    {
+        parent::store($db, $vCenter);
+
+        // We might see VmStartingEvent but no VmPoweredOffEvent
+        $db->update('virtual_machine', [
+            'runtime_power_state' => 'poweredOn'
+        ], $db->quoteInto('uuid = ?', $vCenter->makeBinaryGlobalUuid($this->vm->vm->_)));
+    }
 }
