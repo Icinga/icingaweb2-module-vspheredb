17374f3d2e33c5697287c40263fd3668e4c12f42
Thomas Gelf <thomas@gelf.net>
2018-10-27 17:17:54 +0200

    VmsTable: re-organize columns, disable perf

--- library/Vspheredb/Web/Table/Objects/VmsTable.php
+++ library/Vspheredb/Web/Table/Objects/VmsTable.php
@@ -4,9 +4,9 @@ namespace Icinga\Module\Vspheredb\Web\Table\Objects;
 
 use Icinga\Date\DateFormatter;
 use Icinga\Module\Vspheredb\Web\Widget\DelayedPerfdataRenderer;
+use Icinga\Module\Vspheredb\Web\Widget\MemoryUsage;
 use Icinga\Module\Vspheredb\Web\Widget\PowerStateRenderer;
-use Icinga\Module\Vspheredb\Web\Widget\SimpleUsageBar;
-use Icinga\Util\Format;
+use Icinga\Module\Vspheredb\Format;
 
 class VmsTable extends ObjectsTable
 {
@@ -75,7 +75,6 @@ class VmsTable extends ObjectsTable
 
     protected function initialize()
     {
-        $perf = new DelayedPerfdataRenderer($this->db());
         $powerStateRenderer = new PowerStateRenderer();
         $this->addAvailableColumns([
             $this->createOverallStatusColumn(),
@@ -84,41 +83,44 @@ class VmsTable extends ObjectsTable
             $this->createObjectNameColumn(),
             $this->createColumn('host_name', 'Host', 'h.host_name'),
             $this->createColumn('guest_ip_address', $this->translate('Guest IP'), 'vc.guest_ip_address'),
-            $perf->getDiskColumn()->setDefaultSortDirection('DESC'),
-            $perf->getNetColumn()->setDefaultSortDirection('DESC'),
-            $perf->getCurrentNetColumn()->setDefaultSortDirection('DESC'),
-            $perf->getCurrentDiskColumn()->setDefaultSortDirection('DESC'),
             $this->createColumn('hardware_numcpu', 'CPUs', 'vc.hardware_numcpu')
                 ->setDefaultSortDirection('DESC'),
-            $this->createColumn('hardware_memorymb', 'Memory', 'vc.hardware_memorymb')
-                ->setRenderer(function ($row) {
-                    return $this->formatMb($row->hardware_memorymb);
-                })->setDefaultSortDirection('DESC'),
+            /**
             $this->createColumn('ballooned_memory_mb', 'Balloon', 'vqs.ballooned_memory_mb')
                 ->setRenderer(function ($row) {
-                    return $this->formatMb($row->ballooned_memory_mb);
+                    return Format::mBytes($row->ballooned_memory_mb);
                 })->setDefaultSortDirection('DESC'),
-
-            $this->createColumn('guest_memory_usage_mb', 'Memory Usage', [
+            */
+            $this->createColumn('cpu_usage', 'CPU Usage', 'vqs.overall_cpu_usage')
+                ->setRenderer(function ($row) {
+                    return Format::mhz($row->cpu_usage);
+                })->setDefaultSortDirection('DESC'),
+            $this->createColumn('hardware_memorymb', 'Memory', 'vc.hardware_memorymb')
+                ->setRenderer(function ($row) {
+                    return Format::mBytes($row->hardware_memorymb);
+                })->setDefaultSortDirection('DESC'),
+            $this->createColumn('memory_usage', 'Memory Usage', [
                 'guest_memory_usage_mb' => 'vqs.guest_memory_usage_mb',
+                'host_memory_usage_mb' => 'vqs.host_memory_usage_mb',
                 'hardware_memorymb'     => 'vc.hardware_memorymb',
             ])->setRenderer(function ($row) {
-                $used = $row->guest_memory_usage_mb * 1024 * 1024;
-                $total = $row->hardware_memorymb * 1024 * 1024;
-                $title = sprintf(
-                    '%s / %s',
-                    Format::bytes($used),
-                    Format::bytes($total)
+                return new MemoryUsage(
+                    $row->guest_memory_usage_mb,
+                    $row->hardware_memorymb,
+                    $row->host_memory_usage_mb
                 );
-
-                return new SimpleUsageBar($used, $total, $title);
             })->setSortExpression(
                 '(vqs.guest_memory_usage_mb / vc.hardware_memorymb)'
             )->setDefaultSortDirection('DESC'),
-            $this->createColumn('overall_cpu_usage', 'CPU Usage', 'vqs.overall_cpu_usage')
-                ->setRenderer(function ($row) {
-                    return $this->formatMhz($row->overall_cpu_usage);
-                })->setDefaultSortDirection('DESC'),
+
+            $this->createColumn('host_memory', 'Host Memory Usage', [
+                'host_memory_usage_mb' => 'vqs.host_memory_usage_mb',
+                'hardware_memorymb'     => 'vc.hardware_memorymb',
+            ])->setRenderer(function ($row) {
+                return new MemoryUsage($row->host_memory_usage_mb, $row->hardware_memorymb);
+            })->setSortExpression(
+                '(vqs.host_memory_usage_mb / vc.hardware_memorymb)'
+            )->setDefaultSortDirection('DESC'),
 
 
             $this->createColumn('uptime', $this->translate('Uptime'), [
@@ -130,24 +132,19 @@ class VmsTable extends ObjectsTable
 
                 return DateFormatter::formatDuration($row->uptime);
             }),
-            $this->createColumn('host_memory_usage_mb', 'Host Memory Usage', [
-                'host_memory_usage_mb' => 'vqs.host_memory_usage_mb',
-                'hardware_memorymb'     => 'vc.hardware_memorymb',
-            ])->setRenderer(function ($row) {
-                $used = $row->host_memory_usage_mb * 1024 * 1024;
-                $total = $row->hardware_memorymb * 1024 * 1024;
-                $title = sprintf(
-                    '%s / %s',
-                    Format::bytes($used),
-                    Format::bytes($total)
-                );
+        ]);
 
-                return new SimpleUsageBar($used, $total, $title);
-            })->setSortExpression(
-                '(vqs.host_memory_usage_mb / vc.hardware_memorymb)'
-            )->setDefaultSortDirection('DESC'),
+        // $this->addPerfColumns();
+    }
 
-
+    protected function addPerfColumns()
+    {
+        $perf = new DelayedPerfdataRenderer($this->db());
+        $this->addAvailableColumns([
+            $perf->getDiskColumn()->setDefaultSortDirection('DESC'),
+            $perf->getNetColumn()->setDefaultSortDirection('DESC'),
+            $perf->getCurrentNetColumn()->setDefaultSortDirection('DESC'),
+            $perf->getCurrentDiskColumn()->setDefaultSortDirection('DESC'),
         ]);
     }
 
@@ -157,8 +154,8 @@ class VmsTable extends ObjectsTable
             'overall_status',
             'runtime_power_state',
             'object_name',
-            'hardware_numcpu',
-            'hardware_memorymb'
+            'cpu_usage',
+            'memory_usage',
         ];
     }
 }
