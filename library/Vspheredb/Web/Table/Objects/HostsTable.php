e1ac6c3f14744890ece4b0c263e0ff084ee7b59a
Thomas Gelf <thomas@gelf.net>
2018-10-27 17:15:00 +0200

    HostsTable: reorganize columns, use new renderers

--- library/Vspheredb/Web/Table/Objects/HostsTable.php
+++ library/Vspheredb/Web/Table/Objects/HostsTable.php
@@ -5,11 +5,13 @@ namespace Icinga\Module\Vspheredb\Web\Table\Objects;
 use dipl\Html\Link;
 use Icinga\Date\DateFormatter;
 use Icinga\Module\Vspheredb\DbObject\HostSystem;
+use Icinga\Module\Vspheredb\Web\Widget\CpuUsage;
+use Icinga\Module\Vspheredb\Web\Widget\MemoryUsage;
 use Icinga\Module\Vspheredb\Web\Widget\PowerStateRenderer;
 use Icinga\Module\Vspheredb\Web\Widget\ServiceTagRenderer;
 use Icinga\Module\Vspheredb\Web\Widget\SimpleUsageBar;
 use Icinga\Module\Vspheredb\Web\Widget\SpectreMelddownBiosInfo;
-use Icinga\Util\Format;
+use Icinga\Module\Vspheredb\Format;
 
 class HostsTable extends ObjectsTable
 {
@@ -23,6 +25,36 @@ class HostsTable extends ObjectsTable
             $this->createOverallStatusColumn(),
             $this->createColumn('runtime_power_state', $this->translate('Power'), 'h.runtime_power_state')
                 ->setRenderer($powerStateRenderer),
+            $this->createObjectNameColumn(),
+            $this->createColumn('sysinfo_vendor', $this->translate('Vendor'), 'h.sysinfo_vendor'),
+            $this->createColumn('sysinfo_model', $this->translate('Model'), 'h.sysinfo_model'),
+            $this->createColumn('bios_version', $this->translate('BIOS Version'), 'h.bios_version'),
+            $this->createColumn('bios_release_date', $this->translate('BIOS Release Date'), 'h.bios_release_date')
+                ->setRenderer(function ($row) {
+                    return DateFormatter::formatDate(strtotime($row->bios_release_date));
+                }),
+            $this->createColumn('service_tag', $this->translate('Service Tag'), [
+                'service_tag'    => 'h.service_tag',
+                'sysinfo_vendor' => 'h.sysinfo_vendor',
+            ])->setRenderer($serviceTagRenderer),
+            $this->createColumn('cpu_usage', $this->translate('CPU Usage'), [
+                'cpu_usage' => 'hqs.overall_cpu_usage',
+                'cpu_total' => '(hardware_cpu_cores * hardware_cpu_mhz)',
+            ])->setRenderer(function ($row) {
+                return new CpuUsage($row->cpu_usage, $row->cpu_total);
+            })->setSortExpression(
+                'hqs.overall_cpu_usage / (h.hardware_cpu_cores * h.hardware_cpu_mhz)'
+            )->setDefaultSortDirection('DESC'),
+            $this->createColumn('memory_usage', $this->translate('Memory Usage'), [
+                'hardware_memory_size_mb' => 'h.hardware_memory_size_mb',
+                'memory_usage_mb'         => 'hqs.overall_memory_usage_mb',
+            ])->setRenderer(function ($row) {
+                return new MemoryUsage($row->memory_usage_mb, $row->hardware_memory_size_mb);
+            })->setSortExpression(
+                '(hqs.overall_memory_usage_mb / h.hardware_memory_size_mb)'
+            )->setDefaultSortDirection('DESC'),
+            $this->createColumn('hardware_cpu_cores', $this->translate('CPU Cores'), 'h.hardware_cpu_cores')
+                ->setDefaultSortDirection('DESC'),
             $this->createColumn('vms_overall_status', $this->translate('VM Status'), [
                 'vms_cnt_overall_gray'   => 'vms.vms_cnt_overall_gray',
                 'vms_cnt_overall_green'  => 'vms.vms_cnt_overall_green',
@@ -56,55 +88,20 @@ class HostsTable extends ObjectsTable
                 'vms.vms_cnt_overall_gray',
                 'vms.vms_cnt_overall_green',
             ])->setDefaultSortDirection('DESC'),
-            $this->createObjectNameColumn(),
-            $this->createColumn('sysinfo_vendor', $this->translate('Vendor'), 'h.sysinfo_vendor'),
-            $this->createColumn('sysinfo_model', $this->translate('Model'), 'h.sysinfo_model'),
-            $this->createColumn('bios_version', $this->translate('BIOS Version'), 'h.bios_version'),
-            $this->createColumn('bios_release_date', $this->translate('BIOS Release Date'), 'h.bios_release_date')
-                ->setRenderer(function ($row) {
-                    return DateFormatter::formatDate(strtotime($row->bios_release_date));
-                }),
-            $this->createColumn('service_tag', $this->translate('Service Tag'), [
-                'service_tag'    => 'h.service_tag',
-                'sysinfo_vendor' => 'h.sysinfo_vendor',
-            ])->setRenderer($serviceTagRenderer),
-            $this->createColumn('cpu_usage', $this->translate('CPU Usage'), [
-                'cpu_usage' => 'hqs.overall_cpu_usage',
-                'cpu_total' => '(hardware_cpu_cores * hardware_cpu_mhz)',
-            ])->setRenderer(function ($row) {
-                $title = sprintf('%s / %s MHz', $row->cpu_usage, $row->cpu_total);
-
-                return new SimpleUsageBar($row->cpu_usage, $row->cpu_total, $title);
-            })->setSortExpression(
-                'hqs.overall_cpu_usage / (h.hardware_cpu_cores * h.hardware_cpu_mhz)'
-            )->setDefaultSortDirection('DESC'),
-            $this->createColumn('memory_usage', $this->translate('Memory Usage'), [
-                'hardware_memory_size_mb' => 'h.hardware_memory_size_mb',
-                'memory_usage_mb'         => 'hqs.overall_memory_usage_mb',
-            ])->setRenderer(function ($row) {
-                $used = $row->memory_usage_mb * 1024 * 1024;
-                $total = $row->hardware_memory_size_mb * 1024 * 1024;
-                $title = sprintf(
-                    '%s / %s',
-                    Format::bytes($used),
-                    Format::bytes($total)
-                );
-
-                return new SimpleUsageBar($used, $total, $title);
-            })->setSortExpression(
-                '(hqs.overall_memory_usage_mb / h.hardware_memory_size_mb)'
-            )->setDefaultSortDirection('DESC'),
-            $this->createColumn('hardware_cpu_cores', $this->translate('CPU Cores'), 'h.hardware_cpu_cores')
-                ->setDefaultSortDirection('DESC'),
             $this->createColumn('vms_cnt_cpu', $this->translate('VM CPUs'), 'vms.cnt_cpu')
                 ->setDefaultSortDirection('DESC'),
+            $this->createColumn('pcpu_vcpu_ration', $this->translate('vCPU/pCPU'),
+                '(vms.cnt_cpu / h.hardware_cpu_cores)'
+            )->setRenderer(function ($row) {
+                return sprintf('%.3g:1', $row->pcpu_vcpu_ration);
+            })->setDefaultSortDirection('DESC'),
             $this->createColumn('hardware_memory_size_mb', $this->translate('Memory'), 'h.hardware_memory_size_mb')
                 ->setRenderer(function ($row) {
-                    return Format::bytes($row->hardware_memory_size_mb * 1024 * 1024, Format::STANDARD_IEC);
+                    return Format::mBytes($row->hardware_memory_size_mb);
                 })->setDefaultSortDirection('DESC'),
             $this->createColumn('vms_memorymb', $this->translate('VMs Memory'), 'vms.memorymb')
                 ->setRenderer(function ($row) {
-                    return Format::bytes($row->vms_memorymb * 1024 * 1024, Format::STANDARD_IEC);
+                    return Format::mBytes($row->vms_memorymb);
                 })->setDefaultSortDirection('DESC'),
             $this->createColumn('vms_cnt', $this->translate('VMs'), 'vms.cnt')
                 ->setDefaultSortDirection('DESC'),
