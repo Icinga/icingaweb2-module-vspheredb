3f74510d6c92ec8ee82b06df23b400f3ad1e3b53
Thomas Gelf <thomas@gelf.net>
2018-10-31 08:45:31 +0100

    ConfigWatch: watch resource config for changes

---
+++ library/Vspheredb/Daemon/ConfigWatch.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Daemon;
+
+use Evenement\EventEmitterTrait;
+use Icinga\Application\Config;
+use React\EventLoop\LoopInterface;
+
+/**
+ * ConfigWatch
+ *
+ * Checks every 3 seconds for changed DB resource configuration. Emits a
+ * 'dbConfig' event in case this happens.
+ */
+class ConfigWatch
+{
+    use EventEmitterTrait;
+
+    /** @var string */
+    protected $configFile;
+
+    /** @var string */
+    protected $resourceConfigFile;
+
+    /** @var string|null */
+    protected $dbResourceName;
+
+    /** @var array|null */
+    protected $resourceConfig;
+
+    protected $interval = 3;
+
+    public function __construct($dbResourceName = null)
+    {
+        $this->configFile = Config::module('vspheredb')->getConfigFile();
+        if ($dbResourceName === null) {
+            $this->resourceConfigFile = Config::app('resources')->getConfigFile();
+        } else {
+            $this->dbResourceName = $dbResourceName;
+        }
+    }
+
+    /**
+     * @param LoopInterface $loop
+     */
+    public function run(LoopInterface $loop)
+    {
+        $check = function () {
+            $this->checkForFreshConfig();
+        };
+        $loop->addPeriodicTimer($this->interval, $check);
+        $loop->futureTick($check);
+    }
+
+    protected function checkForFreshConfig()
+    {
+        if ($this->configHasBeenChanged()) {
+            $this->emit('dbConfig', [$this->resourceConfig]);
+        }
+    }
+
+    protected function getResourceName()
+    {
+        if ($this->dbResourceName) {
+            return $this->dbResourceName;
+        } else {
+            return $this->loadDbResourceName();
+        }
+    }
+
+    protected function loadDbResourceName()
+    {
+        $parsed = @parse_ini_file($this->configFile, true);
+        if (isset($parsed['db']['resource'])) {
+            return $parsed['db']['resource'];
+        } else {
+            return null;
+        }
+    }
+
+    protected function loadDbConfigFromDisk($name)
+    {
+        if ($name === null) {
+            return null;
+        }
+
+        $parsed = @parse_ini_file($this->resourceConfigFile, true);
+        if (isset($parsed[$name])) {
+            $section = $parsed[$name];
+            ksort($section);
+
+            return $section;
+        } else {
+            return null;
+        }
+    }
+
+    protected function configHasBeenChanged()
+    {
+        $resource = $this->loadDbConfigFromDisk($this->loadDbResourceName());
+        if ($resource !== $this->resourceConfig) {
+            $this->resourceConfig = $resource;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
