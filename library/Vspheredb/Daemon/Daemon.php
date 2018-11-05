97ed834e011b2d221ac63576b1d51527c0fbe835
Thomas Gelf <thomas@gelf.net>
2018-11-05 03:34:36 +0100

    Daemon: completely new implementation

---
+++ library/Vspheredb/Daemon/Daemon.php
@@ -0,0 +1,400 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Daemon;
+
+use Exception;
+use Icinga\Application\Logger;
+use Icinga\Application\Platform;
+use Icinga\Data\ConfigObject;
+use Icinga\Module\Vspheredb\Db;
+use Icinga\Module\Vspheredb\DbObject\VCenter;
+use Icinga\Module\Vspheredb\DbObject\VCenterServer;
+use Icinga\Module\Vspheredb\LinuxUtils;
+use Icinga\Module\Vspheredb\Rpc\LogProxy;
+use Icinga\Module\Vspheredb\Util;
+use React\EventLoop\Factory as Loop;
+use React\EventLoop\LoopInterface;
+use RuntimeException;
+
+class Daemon
+{
+    use StateMachine;
+
+    /** @var LoopInterface */
+    private $loop;
+
+    /** @var array|null */
+    protected $dbConfig;
+
+    /** @var Db */
+    protected $connection;
+
+    /** @var object */
+    protected $processInfo;
+
+    /** @var bool */
+    protected $shuttingDown = false;
+
+    /** @var ServerRunner[] */
+    protected $running = [];
+
+    protected $delayOnFailed = 5;
+
+    public function __construct()
+    {
+        $this->detectProcessInfo();
+    }
+
+    protected function detectProcessInfo()
+    {
+        $this->processInfo = (object) [
+            'instance_uuid' => Util::generateUuid(),
+            'pid'           => posix_getpid(),
+            'fqdn'          => Platform::getFqdn(),
+            'username'      => Platform::getPhpUser(),
+            'php_version'   => Platform::getPhpVersion(),
+        ];
+    }
+
+    public function run(LoopInterface $loop = null)
+    {
+        $ownLoop = $loop === null;
+        if ($ownLoop) {
+            $loop = Loop::create();
+        }
+
+        $this->loop = $loop;
+        $this->registerSignalHandlers();
+        $this->makeReady();
+        $refresh = function () {
+            $this->refreshConfiguredServers();
+        };
+        $loop->addPeriodicTimer(3, $refresh);
+        $loop->futureTick($refresh);
+
+        if ($ownLoop) {
+            $loop->run();
+        }
+    }
+
+    protected function onConnected()
+    {
+        Logger::info('Database connection has been established');
+        $reconnect = function (Exception $e) {
+            Logger::error($e->getMessage());
+            $this->setState('failed');
+        };
+        $refresh = function () {
+            $this->refreshMyState();
+        };
+        RetryUnless::failing($refresh)
+            ->setInterval(5)
+            ->run($this->loop)
+            ->then($reconnect);
+    }
+
+    protected function onDisconnected()
+    {
+        Logger::info('Database connection has been closed');
+        // state cannot be shutdown
+        if ($this->getState() !== 'shutdown') {
+            $this->reconnectToDb();
+        }
+    }
+
+    protected function makeReady()
+    {
+        // Hint: currently disconnected isn't triggered.
+        $this->onTransition(['started', 'failed', 'disconnected'], 'connected', function () {
+            $this->onConnected();
+        })->onState('disconnected', function () {
+            $this->onDisconnected();
+        })->onTransition([
+            'started',
+            'connected',
+            'disconnected',
+            'failed',
+        ], 'shutdown', function () {
+            $this->shutdown();
+        })->onTransition(['connected', 'started'], 'failed', function () {
+            $this->eventuallyDisconnectFromDb();
+        })->onTransition(['started', 'connected', 'disconnected'], 'failed', function () {
+            $this->onFailed();
+        });
+
+        // External events:
+        $this->runConfigWatch();
+        $this->initializeStateMachine('started');
+    }
+
+    protected function stopRunners()
+    {
+        if (! empty($this->running)) {
+            Logger::info('Stopping remaining child processes');
+        }
+        foreach ($this->running as $serverRunner) {
+            $serverRunner->stop();
+        }
+
+        foreach (array_keys($this->running) as $id) {
+            unset($this->running[$id]);
+        }
+        $this->running = [];
+        gc_collect_cycles();
+    }
+
+    protected function onFailed()
+    {
+        $delay = $this->delayOnFailed;
+        Logger::warning("Failed. Reconnecting in ${delay}s");
+        $this->loop->addTimer($delay, function () {
+            $this->reconnectToDb();
+        });
+        $this->stopRunners();
+    }
+
+    protected function reconnectToDb()
+    {
+        $this->eventuallyDisconnectFromDb();
+        RetryUnless::succeeding(function () {
+            if ($this->dbConfig === null) {
+                throw new RuntimeException('Got no valid DB configuration');
+            }
+
+            return $this->connectToDb($this->dbConfig);
+        })->slowDownAfter(5, 15)->run($this->loop)->then(function (Db $connection) {
+            $this->connection = $connection;
+            $this->setState('connected');
+        });
+    }
+
+    protected function connectToDb($config)
+    {
+        $connection = new Db(new ConfigObject($config));
+        $connection->getDbAdapter()->getConnection();
+
+        return $connection;
+    }
+
+    protected function eventuallyDisconnectFromDb()
+    {
+        if ($this->connection !== null) {
+            try {
+                $this->connection->getDbAdapter()->closeConnection();
+            } catch (Exception $e) {
+                Logger::error(
+                    'Ignored an error while closing the DB connection: '
+                    . $e->getMessage()
+                );
+            }
+            $this->connection = null;
+        }
+    }
+
+    protected function runConfigWatch()
+    {
+        $config = new ConfigWatch();
+        $config->on('dbConfig', function ($config) {
+            if ($config === null) {
+                if ($this->dbConfig === null) {
+                    Logger::error('Got no valid DB configuration');
+                } else {
+                    Logger::error('There is no longer a valid DB configuration');
+                    $this->dbConfig = $config;
+                    $this->reconnectToDb();
+                }
+            } else {
+                Logger::info('DB configuration loaded');
+                $this->dbConfig = $config;
+                $this->reconnectToDb();
+            }
+        });
+        $config->run($this->loop);
+    }
+
+    protected function registerSignalHandlers()
+    {
+        $func = function ($signal) use (&$func) {
+            $this->shutdownWithSignal($signal, $func);
+        };
+        $this->loop->addSignal(SIGINT, $func);
+        $this->loop->addSignal(SIGTERM, $func);
+    }
+
+    protected function shutdownWithSignal($signal, &$func)
+    {
+        $this->loop->removeSignal($signal, $func);
+        $this->setState('shutdown');
+    }
+
+    protected function shutdown()
+    {
+        try {
+            Logger::info('Shutting down');
+            $this->eventuallyDisconnectFromDb();
+        } catch (Exception $e) {
+            Logger::error(
+                'Failed to safely shutdown, stopping anyways: %s',
+                $e->getMessage()
+            );
+        }
+        $this->loop->stop();
+    }
+
+    /**
+     * @throws \Zend_Db_Adapter_Exception
+     */
+    protected function refreshMyState()
+    {
+        if ($this->connection === null) {
+            return;
+        }
+        $db = $this->connection->getDbAdapter();
+        $updated = $db->update('vspheredb_daemon', [
+            'instance_uuid' => $this->processInfo->instance_uuid,
+            'ts_last_refresh' => Util::currentTimestamp(),
+            'process_info'    => json_encode($this->getProcessInfo()),
+        ], $db->quoteInto('instance_uuid = ?', $this->processInfo->instance_uuid));
+
+        if (! $updated) {
+            $this->insertMyState($db);
+        }
+    }
+
+    /**
+     * @param \Zend_Db_Adapter_Abstract $db
+     * @throws \Zend_Db_Adapter_Exception
+     */
+    protected function insertMyState(\Zend_Db_Adapter_Abstract $db)
+    {
+        $db->insert('vspheredb_daemon', [
+            'instance_uuid' => $this->processInfo->instance_uuid,
+            'ts_last_refresh' => Util::currentTimestamp(),
+            'process_info'    => json_encode($this->getProcessInfo()),
+        ] + (array) $this->processInfo);
+    }
+
+    protected function truncateDaemonLog(\Zend_Db_Adapter_Abstract $db)
+    {
+
+    }
+
+    protected function getProcessInfo()
+    {
+        global $argv;
+        $pid = $this->processInfo->pid;
+        $info = (object) [$pid => (object) [
+            'command' => implode(' ', $argv),
+            'running' => true,
+            'memory'  => LinuxUtils::getMemoryUsageForPid($pid)
+        ]];
+
+        foreach ($this->running as $runner) {
+            foreach ($runner->getProcessInfo() as $pid => $details) {
+                $info->$pid = $details;
+            }
+        }
+
+        return $info;
+    }
+
+    /**
+     * @param VCenter[] $vCenters
+     * @param VCenterServer[] $vServers
+     */
+    protected function checkRequiredProcesses($vCenters, $vServers)
+    {
+        $required = [];
+        $candidates = [];
+        foreach ($vServers as $id => $server) {
+            $id = (int) $id;
+            $vCenterId = $server->get('vcenter_id');
+            if ($vCenterId === null) {
+                $required[$id] = $server;
+            } else {
+                $candidates[$vCenterId][$id] = $server;
+            }
+        }
+
+        foreach ($vCenters as $id => $vCenter) {
+            if (isset($candidates[$id])) {
+                // Pick the first one. TODO: mark failing ones, try others
+                foreach ($candidates[$id] as $serverId => $server) {
+                    $required[$serverId] = $server;
+                    break;
+                }
+            } else {
+                Logger::info(sprintf('vCenter ID=%s has no Server', $id));
+            }
+        }
+
+        foreach ($required as $id => $vServer) {
+            if (! isset($this->running[$id])) {
+                Logger::info("$id is now running");
+                $this->running[$id] = $this->runServer($vServer);
+            }
+        }
+
+        foreach ($this->running as $id => $serverRunner) {
+            if (! isset($required[$id])) {
+                $this->running[$id]->stop();
+                unset($this->running[$id]);
+            }
+        }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @param VCenterServer $server
+     * @return ServerRunner
+     */
+    protected function runServer(VCenterServer $server)
+    {
+        $runner = new ServerRunner($server);
+        $vCenterId = $server->get('vcenter_id');
+        Logger::info("Starting for $vCenterId");
+        /** @var Db $connection */
+        $connection = $server->getConnection();
+        $logProxy = new LogProxy($connection, $this->processInfo->instance_uuid);
+        $runner->forwardLog($logProxy);
+        $runner->run($this->loop);
+        $runner->on('processStopped', function ($pid) use ($vCenterId) {
+            Logger::info("Pid $pid stopped");
+            try {
+                $this->refreshMyState();
+            } catch (Exception $e) {
+                $this->eventuallyDisconnectFromDb();
+            }
+        });
+        $runner->on('failed', function ($pid) use ($vCenterId) {
+            if (isset($this->running[$vCenterId])) {
+                Logger::info("Pid $pid failed, killing runner");
+                $this->running[$vCenterId]->stop();
+                unset($this->running[$vCenterId]);
+                gc_collect_cycles();
+            }
+            // $this->setState('failed');
+        });
+
+        return $runner;
+    }
+
+    protected function refreshConfiguredServers()
+    {
+        try {
+            if ($this->getState() === 'connected') {
+                $vCenters = VCenter::loadAll($this->connection, null, 'id');
+                $vServers = VCenterServer::loadAll($this->connection, null, 'id');
+                $this->checkRequiredProcesses($vCenters, $vServers);
+            } else {
+                $this->stopRunners();
+            }
+        } catch (Exception $e) {
+            Logger::error($e->getMessage());
+            $this->setState('failed');
+        } catch (\Error $e) {
+            Logger::error($e->getMessage());
+            $this->setState('failed');
+        }
+    }
+}
---
+++ library/Vspheredb/Daemon/SyncRunner.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Daemon;
+
+use Icinga\Application\Logger;
+use Icinga\Module\Vspheredb\DbObject\Datastore;
+use Icinga\Module\Vspheredb\DbObject\HostSystem;
+use Icinga\Module\Vspheredb\DbObject\VCenter;
+use Icinga\Module\Vspheredb\DbObject\VirtualMachine;
+use Icinga\Module\Vspheredb\EventManager;
+use Icinga\Module\Vspheredb\Sync\SyncHostHardware;
+use Icinga\Module\Vspheredb\Sync\SyncHostSensors;
+use Icinga\Module\Vspheredb\Sync\SyncManagedObjectReferences;
+use Icinga\Module\Vspheredb\Sync\SyncPerfCounterInfo;
+use Icinga\Module\Vspheredb\Sync\SyncPerfCounters;
+use Icinga\Module\Vspheredb\Sync\SyncQuickStats;
+use Icinga\Module\Vspheredb\Sync\SyncVmDatastoreUsage;
+use Icinga\Module\Vspheredb\Sync\SyncVmDiskUsage;
+use Icinga\Module\Vspheredb\Sync\SyncVmHardware;
+use Icinga\Module\Vspheredb\Sync\SyncVmSnapshots;
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+
+class SyncRunner
+{
+    /** @var VCenter */
+    protected $vCenter;
+
+    /** @var EventManager */
+    protected $eventManager;
+
+    /** @var LoopInterface */
+    protected $loop;
+
+    protected $availableTasks = [];
+
+    protected $immediateTasks = [];
+
+    /** @var Deferred */
+    protected $deferred;
+
+    /**
+     * SyncRunner constructor.
+     * @param VCenter $vCenter
+     */
+    public function __construct(VCenter $vCenter)
+    {
+        $this->vCenter = $vCenter;
+        $this->availableTasks = [
+            'moRefs' => function () {
+                Logger::info('Task: moRefs');
+                (new SyncManagedObjectReferences($this->vCenter))->sync();
+            },
+            'quickStats' => function () {
+                Logger::info('Task: quickStats');
+                (new SyncQuickStats($this->vCenter))->run();
+            },
+            'hostSystems' => function () {
+                Logger::info('Task: hostSystems');
+                HostSystem::syncFromApi($this->vCenter);
+            },
+            'virtualMachines' => function () {
+                Logger::info('Task: virtualMachines');
+                VirtualMachine::syncFromApi($this->vCenter);
+            },
+            'dataStores' => function () {
+                Logger::info('Task: dataStores');
+                Datastore::syncFromApi($this->vCenter);
+            },
+            'hostHardware' => function () {
+                Logger::info('Task: hostHardware');
+                (new SyncHostHardware($this->vCenter))->run();
+            },
+            'hostSensors' => function () {
+                Logger::info('Task: hostSensors');
+                (new SyncHostSensors($this->vCenter))->run();
+            },
+            'vmHardware' => function () {
+                Logger::info('Task: vmHardware');
+                (new SyncVmHardware($this->vCenter))->run();
+            },
+            'vmDiskUsage' => function () {
+                Logger::info('Task: vmDiskUsage');
+                (new SyncVmDiskUsage($this->vCenter))->run();
+            },
+            'vmDatastoreUsage' => function () {
+                Logger::info('Task: vmDatastoreUsage');
+                (new SyncVmDatastoreUsage($this->vCenter))->run();
+            },
+            'vmSnapshots' => function () {
+                Logger::info('Task: vmSnapshots');
+                (new SyncVmSnapshots($this->vCenter))->run();
+            },
+            'eventStream' => function () {
+                Logger::info('Task: eventStream');
+                $this->streamEvents();
+            },
+            'perfCounters' => function () {
+                Logger::info('Task: perfCounters');
+                (new SyncPerfCounters($this->vCenter))->run();
+            },
+            'perfCounterInfo' => function () {
+                Logger::info('Task: perfCounterInfo');
+                (new SyncPerfCounterInfo($this->vCenter))->run();
+            },
+        ];
+    }
+
+    /**
+     * @param LoopInterface $loop
+     */
+    public function run(LoopInterface $loop)
+    {
+        $this->deferred = new Deferred();
+        $this->loop = $loop;
+        $initialSync = [
+            'moRefs',
+            'perfCounterInfo',
+            'hostSystems',
+            'virtualMachines',
+            'quickStats',
+            'dataStores',
+            'vmDatastoreUsage',
+            'vmDiskUsage',
+            'vmSnapshots',
+            'vmHardware',
+            'hostHardware',
+            'hostSensors',
+        ];
+
+        $schedule = [
+            [2, ['eventStream']],
+            [900, [
+                'moRefs',
+                'hostSystems',
+                'virtualMachines',
+            ]],
+            [1800, [
+                'vmSnapshots',
+                'vmHardware',
+            ]],
+            [7200, [
+                'hostHardware',
+                'hostSensors',
+            ]],
+            [300, [
+                'vmDatastoreUsage',
+                'vmDiskUsage',
+            ]],
+            [90, [
+                'quickStats',
+            ]],
+        ];
+
+        $this->runTasks($initialSync);
+        $loop->addTimer(0.1, function () {
+            $this->runNextImmediateTask();
+        });
+        foreach ($schedule as $pair) {
+            $loop->addPeriodicTimer($pair[0], $this->callRunTasks($pair[1]));
+        }
+
+        return $this->deferred->promise();
+    }
+
+    public function runNextImmediateTask()
+    {
+        $task = array_shift($this->immediateTasks);
+        if ($task) {
+            $func = $this->availableTasks[$task];
+            try {
+                $func();
+            } catch (\Exception $e) {
+                Logger::error($e);
+                $this->loop->addTimer(0.5, function () {
+                    $this->deferred->reject();
+                });
+                $this->loop->addTimer(5, function () {
+                    $this->runNextImmediateTask();
+                });
+            }
+            $this->loop->addTimer(0.1, function () {
+                $this->runNextImmediateTask();
+            });
+        } else {
+            $this->loop->addTimer(1, function () {
+                $this->runNextImmediateTask();
+            });
+        }
+    }
+
+    protected function callRunTasks($tasks)
+    {
+        return function () use ($tasks) {
+            $this->runTasks($tasks);
+        };
+    }
+
+    protected function runTasks($tasks)
+    {
+        foreach ($tasks as $task) {
+            $this->immediateTasks[$task] = $task;
+        }
+    }
+
+    /**
+     * @return EventManager
+     * @throws \Icinga\Exception\AuthenticationException
+     * @throws \Icinga\Exception\NotFoundError
+     * @throws \Zend_Db_Select_Exception
+     */
+    protected function eventManager()
+    {
+        if ($this->eventManager === null) {
+            $this->eventManager = $this->vCenter->getApi()->eventManager()
+                ->persistFor($this->vCenter);
+        }
+
+        return $this->eventManager;
+    }
+
+    /**
+     * @throws \Icinga\Exception\AuthenticationException
+     * @throws \Icinga\Exception\ConfigurationError
+     * @throws \Icinga\Exception\NotFoundError
+     * @throws \Zend_Db_Exception
+     * @throws \Zend_Db_Select_Exception
+     */
+    public function streamEvents()
+    {
+        $cnt = $this->eventManager()->streamToDb();
+        if ($cnt < 1000) {
+            Logger::debug('Got %d events', $cnt);
+        } else {
+            Logger::debug('Got %d event(s), there might be more', $cnt);
+        }
+    }
+}
