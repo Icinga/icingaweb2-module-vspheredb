b8f1a3edbdfe8e8f12b6a81b66de5e478547d1fe
Thomas Gelf <thomas@gelf.net>
2018-11-06 02:14:17 +0100

    Format: better-looking MHz formatting

--- library/Vspheredb/Format.php
+++ library/Vspheredb/Format.php
@@ -19,11 +19,11 @@ class Format
     public static function mhz($mhz)
     {
         if ($mhz > 1000000) {
-            return sprintf('%.2f THz', $mhz / 1000000);
+            return sprintf('%.3G THz', $mhz / 1000000);
         } elseif ($mhz > 1000) {
-            return sprintf('%.2f GHz', $mhz / 1000);
+            return sprintf('%.3G GHz', $mhz / 1000);
         } else {
-            return sprintf('%.2f MHz', $mhz);
+            return sprintf('%.3G MHz', $mhz);
         }
     }
 }
