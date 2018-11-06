6ce26702113c91696619fce42dc1d9ab585030e2
Thomas Gelf <thomas@gelf.net>
2018-11-06 02:18:56 +0100

    configuration: temporarily disable some links

--- configuration.php
+++ configuration.php
@@ -18,18 +18,18 @@ $section->add(N_('Datastores'))
 $section->add(N_('Virtual Machines'))
     ->setUrl('vspheredb/vms')
     ->setPriority(40);
-$section->add(N_('Anomalies'))
-    ->setUrl('vspheredb/anomalies')
-    ->setPriority(45);
+// $section->add(N_('Anomalies'))
+//     ->setUrl('vspheredb/anomalies')
+//     ->setPriority(45);
 $section->add(N_('Event History'))
     ->setUrl('vspheredb/events')
     ->setPriority(49);
 $section->add(N_('Alarm History'))
     ->setUrl('vspheredb/alarms')
     ->setPriority(49);
-$section->add(N_('Performance Counter'))
-    ->setUrl('vspheredb/configuration/counters')
-    ->setPriority(60);
-$section->add(N_('Top VMs'))
-    ->setUrl('vspheredb/top/vms')
-    ->setPriority(70);
+// $section->add(N_('Performance Counter'))
+//     ->setUrl('vspheredb/configuration/counters')
+//     ->setPriority(60);
+// $section->add(N_('Top VMs'))
+//     ->setUrl('vspheredb/top/vms')
+//     ->setPriority(70);
