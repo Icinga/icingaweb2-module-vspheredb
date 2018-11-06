205c06d73e181942749129b90fbdda9d99654599
Thomas Gelf <thomas@gelf.net>
2018-11-06 04:01:12 +0100

    Configuration: new controller

--- application/controllers/ConfigurationController.php
+++ application/controllers/ConfigurationController.php
@@ -2,11 +2,73 @@
 
 namespace Icinga\Module\Vspheredb\Controllers;
 
+use dipl\Html\Html;
+use Icinga\Module\Vspheredb\Db\Migrations;
+use Icinga\Module\Vspheredb\Web\Form\ChooseDbResourceForm;
+use Icinga\Module\Vspheredb\Web\Tabs\MainTabs;
+use Icinga\Module\Vspheredb\Web\Form\ApplyMigrationsForm;
 use Icinga\Module\Vspheredb\Web\Controller;
 use Icinga\Module\Vspheredb\Web\Table\PerformanceCounterTable;
 
 class ConfigurationController extends Controller
 {
+    /**
+     * @throws \Icinga\Security\SecurityException
+     */
+    public function indexAction()
+    {
+        $this->assertPermission('vspheredb/admin');
+        $this->addTitle($this->translate('Main Configuration'));
+        $this->content()->add(
+            ChooseDbResourceForm::load()->handleRequest()
+        );
+
+        if ($this->Config()->get('db', 'resource')) {
+            $db = $this->db();
+            $this->tabs(new MainTabs($db))->activate('configuration');
+            $migrations = new Migrations($db);
+
+            if ($migrations->hasSchema()) {
+                if ($migrations->hasPendingMigrations()) {
+                    $this->content()->add(
+                        ApplyMigrationsForm::load()
+                            ->setMigrations($migrations)
+                            ->handleRequest()
+                    );
+                }
+            } else {
+                if ($migrations->hasModuleRelatedTable()) {
+                    $this->content()->add(Html::tag('p', [
+                        'class' => 'error'
+                    ], $this->translate(
+                        'The chosen Database resource contains related tables,'
+                        . ' but the schema is not complete. In case you tried'
+                        . ' a pre-release version of this module please drop'
+                        . ' this database and start with a fresh new one.'
+                    )));
+
+                    return;
+                } elseif ($migrations->hasAnyTable()) {
+                    $this->content()->add(Html::tag('p', [
+                        'class' => 'warning'
+                    ], $this->translate(
+                        'The chosen Database resource already contains tables. You'
+                        . ' might want to continue with this DB resource, but we'
+                        . ' strongly suggest to use an empty dedicated DB for this'
+                        . ' module.'
+                    )));
+                }
+                $this->content()->add(
+                    ApplyMigrationsForm::load()
+                        ->setMigrations($migrations)
+                        ->handleRequest()
+                );
+            }
+        } else {
+            $this->tabs(new MainTabs())->activate('configuration');
+        }
+    }
+
     public function countersAction()
     {
         $this->addSingleTab('Counters');
