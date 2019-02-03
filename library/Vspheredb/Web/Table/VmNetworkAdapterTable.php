b7baf1f5e03c3b34fdd8cab8954f5b4de26665f1
Thomas Gelf <thomas@gelf.net>
2019-02-03 23:51:17 +0100

    VmNetworkAdapterTable: add perfdata images

--- library/Vspheredb/Web/Table/VmNetworkAdapterTable.php
+++ library/Vspheredb/Web/Table/VmNetworkAdapterTable.php
@@ -2,32 +2,90 @@
 
 namespace Icinga\Module\Vspheredb\Web\Table;
 
-use Icinga\Module\Vspheredb\Db;
-use Icinga\Module\Vspheredb\DbObject\Datastore;
+use dipl\Html\Html;
+use dipl\Html\Img;
 use Icinga\Module\Vspheredb\DbObject\VirtualMachine;
-use Icinga\Module\Vspheredb\Web\Widget\DatastoreUsage;
-use Icinga\Util\Format;
 use dipl\Html\Link;
 use dipl\Web\Table\ZfQueryBasedTable;
 
 class VmNetworkAdapterTable extends ZfQueryBasedTable
 {
+    protected $defaultAttributes = [
+        'class' => 'common-table',
+        'data-base-target' => '_next',
+    ];
+
     /** @var VirtualMachine */
     protected $vm;
+
+    /** @var string */
+    protected $moref;
 
     public function __construct(VirtualMachine $vm)
     {
         $this->vm = $vm;
+        $this->moref = $this->vm->object()->get('moref');
         parent::__construct($vm->getConnection());
     }
 
     public function getColumnsToBeRendered()
     {
         return [
-            $this->translate('Interface'),
-            $this->translate('MAC Address'),
-            $this->translate('Port'),
+            // TODO: no padding in th on our left!
+            Html::tag('h2', [
+                'class' => 'icon-sitemap',
+                'style' => 'margin: 0;'
+            ], $this->translate('Network')),
+            ''
         ];
+    }
+
+    protected function prepareImg($device, $template)
+    {
+        $width = 340 + 170;
+        $height = 180;
+        $start = strtotime('2019-01-31 08:50:00');
+        $end = strtotime('2019-01-31 16:00:00');
+        $params = [
+            'file'     => sprintf('%s/iface%s.rrd', $this->moref, $device),
+            'height'   => $height,
+            'width'    => $width,
+            'rnd'      => floor(time() / 20),
+            'format'   => 'png',
+            'start'    => $start,
+            'end'      => $end,
+        ];
+        $attrs = [
+            'height' => $height,
+            'width'  => $width,
+            //'align'  => 'right',
+            'style' => 'float: right;'
+            // 'style'  => 'border-bottom: 1px solid rgba(0, 0, 0, 0.3); border-left: 1px solid rgba(0, 0, 0, 0.3);'
+        ];
+
+        return Img::create('rrd/graph/img', $params + [
+                'template' => $template,
+            ], $attrs);
+    }
+
+    protected function wrapImage($title, $device, $template)
+    {
+        return Html::tag('div', [
+                'style' => 'display: inline-block; margin-left: 2em;'
+        ], [
+            Html::tag('strong', [
+                'style' => 'display: block; padding-left: 3em'
+            ], $title),
+            $this->prepareImg($device, $template),
+        ]);
+    }
+
+    protected function colorLegend($color)
+    {
+        return Html::tag('div', [
+            'style' => "    border: 1px solid rgba(0, 0, 0, 0.3); background-color: $color;"
+                . ' width: 0.8em; height: 0.8em; margin: 0.1em; display: inline-block; vertical-align: middle;'
+        ]);
     }
 
     public function renderRow($row)
@@ -42,10 +100,31 @@ class VmNetworkAdapterTable extends ZfQueryBasedTable
                 ['data-base-target' => '_next']
             ), ': ' . $row->port_key];
         }
+
         return $this::row([
-            $row->label,
-            $row->mac_address,
-            $portGroup,
+            [
+                Html::tag('strong', $row->label),
+                Html::tag('br'),
+                $this->translate('MAC Address') . ': ' . $row->mac_address,
+                Html::tag('br'),
+                $this->translate('Port'),
+                ': ',
+                $portGroup
+            ], [
+                $this->wrapImage(Html::sprintf(
+                    $this->translate('Throughput (bits/s, %s RX / %s TX)'),
+                    $this->colorLegend('#57985B'),
+                    $this->colorLegend('#0095BF')
+                ), $row->hardware_key, 'vSphereDB-vmIfTraffic'),
+                $this->wrapImage(Html::sprintf(
+                    $this->translate('Packets (%s / %s Unicast, %s BCast, %s MCast, %s Dropped)'),
+                    $this->colorLegend('#57985B'),
+                    $this->colorLegend('#0095BF'),
+                    $this->colorLegend('#EE55FF'),
+                    $this->colorLegend('#FF9933'),
+                    $this->colorLegend('#FF5555')
+                ), $row->hardware_key, 'vSphereDB-vmIfPackets'),
+            ]
         ]);
     }
 
