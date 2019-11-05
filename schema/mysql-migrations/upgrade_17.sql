ff3f426eda8793c63ea7e12091b5e40f576dcb97
Thomas Gelf <thomas@gelf.net>
2019-11-05 16:11:55 +0100

    schema/mysql: fix migration

--- schema/mysql-migrations/upgrade_17.sql
+++ schema/mysql-migrations/upgrade_17.sql
@@ -3,3 +3,6 @@ ALTER TABLE host_system
   MODIFY sysinfo_vendor VARCHAR(64) NULL DEFAULT NULL,
   MODIFY sysinfo_model VARCHAR(64) NULL DEFAULT NULL;
 
+INSERT INTO vspheredb_schema_migration
+(schema_version, migration_time)
+VALUES (17, NOW());
