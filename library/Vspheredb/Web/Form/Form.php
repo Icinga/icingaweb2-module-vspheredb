7a1fbf7a4a38e39e94199ada280b5752d33a3f79
Thomas Gelf <thomas@gelf.net>
2019-11-05 15:36:43 +0100

    Form: render web2.7-like forms

--- library/Vspheredb/Web/Form/Form.php
+++ library/Vspheredb/Web/Form/Form.php
@@ -3,9 +3,9 @@
 namespace Icinga\Module\Vspheredb\Web\Form;
 
 use gipfl\Translation\TranslationHelper;
+use Icinga\Application\Version;
 use Icinga\Module\Vspheredb\Validator\PhpSessionBasedCsrfTokenValidator;
 use ipl\Html\Form as iplForm;
-use ipl\Html\FormDecorator\DdDtDecorator;
 use ipl\Html\FormElement\HiddenElement;
 use ipl\Html\Html;
 use RuntimeException;
@@ -13,6 +13,8 @@ use RuntimeException;
 class Form extends iplForm
 {
     use TranslationHelper;
+
+    protected $formNameElementName = '__FORM_NAME';
 
     public function ensureAssembled()
     {
@@ -30,15 +32,34 @@ class Form extends iplForm
 
     protected function prepareWebForm()
     {
+        $this->addFormNameElement();
         if ($this->getMethod() === 'POST') {
             $this->addCsrfElement();
-            $this->setupStyling();
         }
+        $this->setupStyling();
+    }
+
+    protected function getUniqueFormName()
+    {
+        return \get_class($this);
+    }
+
+    protected function addFormNameElement()
+    {
+        $this->addElement('hidden', $this->formNameElementName, [
+            'value' => $this->getUniqueFormName()
+        ]);
     }
 
     protected function setupStyling()
     {
-        $this->setDefaultElementDecorator(new DdDtDecorator());
+        if (\version_compare(Version::VERSION, '2.7.0', '<')) {
+            $this->getAttributes()->add('class', 'form-compat26');
+        }
+        $this->addAttributes([
+            'class' => 'icinga-form icinga-controls'
+        ]);
+        $this->setDefaultElementDecorator(new LegacyDecorator());
     }
 
     protected function addCsrfElement()
@@ -57,6 +78,44 @@ class Form extends iplForm
         }
     }
 
+    public function getSentValue($name, $default = null)
+    {
+        $request = $this->getRequest();
+        if ($request === null) {
+            throw new RuntimeException(
+                "It's impossible to access SENT values with no request"
+            );
+        }
+        if ($request->getMethod() === 'POST') {
+            $params = $request->getParsedBody();
+        } elseif ($this->getMethod() === 'GET') {
+            \parse_str($request->getUri()->getQuery(), $params);
+        } else {
+            $params = [];
+        }
+
+        if (\array_key_exists($name, $params)) {
+            return $params[$name];
+        } else {
+            return $default;
+        }
+    }
+
+    public function onSuccess()
+    {
+        // Do not show default success message
+    }
+
+    public function hasBeenSent()
+    {
+        if (parent::hasBeenSent()) {
+            return $this->getSentValue($this->formNameElementName)
+                === $this->getUniqueFormName();
+        } else {
+            return false;
+        }
+    }
+
     protected function generateCsrfValue()
     {
         $seed = \mt_rand();
@@ -69,11 +128,14 @@ class Form extends iplForm
     public function onError()
     {
         foreach ($this->getMessages() as $message) {
-            $this->prepend(Html::tag('p', ['class' => 'error'], $message));
+            if ($message instanceof \Exception) {
+                $message = $message->getMessage();
+            }
+            $this->prepend(Html::tag('p', ['class' => 'state-hint error'], $message));
         }
         foreach ($this->getElements() as $element) {
             foreach ($element->getMessages() as $message) {
-                $this->prepend(Html::tag('p', ['class' => 'error'], $message));
+                $this->prepend(Html::tag('p', ['class' => 'state-hint error'], $message));
             }
         }
     }
