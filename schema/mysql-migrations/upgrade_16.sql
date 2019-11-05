dce5a438a0afb796667a341961fb93ea086175e0
Thomas Gelf <thomas@gelf.net>
2019-11-05 18:35:26 +0100

    schema/mysql: drop index in a fail-safe way
    Not all installations had that index

--- schema/mysql-migrations/upgrade_16.sql
+++ schema/mysql-migrations/upgrade_16.sql
@@ -1,5 +1,19 @@
-ALTER TABLE monitoring_connection
-  DROP INDEX priority;
+SET @stmt = (SELECT IF(
+    (SELECT EXISTS(
+        SELECT * FROM information_schema.table_constraints
+        WHERE
+            table_schema   = DATABASE()
+            AND table_name = 'monitoring_connection'
+            AND constraint_name = 'priority'
+    )),
+    'ALTER TABLE monitoring_connection DROP FOREIGN KEY priority',
+    'SELECT 1'
+));
+
+PREPARE stmt FROM @stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+SET @stmt = NULL;
 
 INSERT INTO vspheredb_schema_migration
   (schema_version, migration_time)
