e8d1b871c0c82ae5361e5594840bdbb453aecf46
Dirk Goetz <dirk.goetz@netways.de>
2019-11-27 13:26:44 +0100

    VirtualMachine: add link to console via VMRC

--- library/Vspheredb/Web/Table/Object/VmEssentialInfoTable.php
+++ library/Vspheredb/Web/Table/Object/VmEssentialInfoTable.php
@@ -103,6 +103,10 @@ class VmEssentialInfoTable extends NameValueTable
 
         if ($vm->get('guest_id')) {
             $this->addNameValuePairs([
+                $this->translate('Console') => $this->linkToVMRC(
+                    $vm->object()->get('object_name'),
+                    $vm->object()->get('moref')
+                ),
                 $this->translate('Guest OS') => $guest,
                 $this->translate('Guest IP') => $vm->get('guest_ip_address') ?: '-',
                 $this->translate('Guest Hostname') => $vm->get('guest_host_name') ?: '-',
@@ -210,6 +214,30 @@ class VmEssentialInfoTable extends NameValueTable
             'target' => '_blank',
             'title' => $this->translate('Jump to the Managed Object browser')
         ], $moRef);
+    }
+
+    protected function linkToVMRC($name, $moRef)
+    {
+        try {
+            $server = $this->vCenter->getFirstServer();
+        } catch (NotFoundError $e) {
+            return [
+                Icon::create('warning-empty', [
+                    'class' => 'red'
+                ]),
+                ' ',
+                $this->translate('No related vServer has been configured')
+            ];
+        }
+        return Html::tag('a', [
+            'href' => sprintf(
+                'vmrc://%s/?moid=%s',
+                $server->get('host'),
+                rawurlencode($moRef)
+            ),
+            'target' => '_self',
+            'title' => $this->translate('Open console with VMRC')
+        ], $name);
     }
 
     protected function getConnectionStateDetails($state)
