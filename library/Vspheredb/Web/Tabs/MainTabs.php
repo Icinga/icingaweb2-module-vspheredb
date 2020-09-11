c37fac0a229a93be1dee0ff0ad6e32d4d9884a8b
Thomas Gelf <thomas@gelf.net>
2020-09-11 16:38:50 +0200

    MainTabs: do not show monitoring with no db

--- library/Vspheredb/Web/Tabs/MainTabs.php
+++ library/Vspheredb/Web/Tabs/MainTabs.php
@@ -63,10 +63,14 @@ class MainTabs extends Tabs
             $this->add('configuration', [
                 'label' => $this->translate('Configuration'),
                 'url'   => 'vspheredb/configuration',
-            ])->add('monitoring', [
-                'label' => $this->translate('Monitoring'),
-                'url'   => 'vspheredb/configuration/monitoring',
             ]);
+
+            if ($migrations && $migrations->hasSchema()) {
+                $this->add('monitoring', [
+                    'label' => $this->translate('Monitoring'),
+                    'url' => 'vspheredb/configuration/monitoring',
+                ]);
+            }
         }
         if ($migrations && $migrations->hasSchema()) {
             $this->add('daemon', [
