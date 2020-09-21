46e187da0c81729db1eda26b5a5ea0f2301d33d0
Thomas Gelf <thomas@gelf.net>
2020-09-21 11:16:29 +0200

    public/js: remove obsolete code

--- public/js/module.js
+++ public/js/module.js
@@ -12,31 +12,13 @@
 
     Vspheredb.prototype = {
         initialize: function () {
-            this.module.on('sparklineRegionChange', '.overspark', this.change);
-            this.module.on('mouseleave', '.overspark', this.leave);
-            this.module.on('render', this.rendered);
             this.module.on('mouseover', 'thead tr', this.checkForHeaderHref);
-            this.module.on('mouseover', '.inline-perf-container', this.inlinePerfSmall);
-            this.module.on('mouseout', '.inline-perf-container', this.inlinePerfSmallOut);
             this.module.on('mouseover', '.content [href]', this.highlightMatchingLinks);
             this.module.on('mouseout', '.content [href]', this.removeMatchingLinksHighlight);
             this.module.on('keydown', '', this.keyDown);
             this.module.on('keyup', '', this.keyUp);
             this.module.on('keyup', 'form.quicksearch input.search', this.keyUpInQuickSearch);
-            $(document).keydown(this.bodyKeyDown);
-        },
-
-        inlinePerfSmall: function (ev) {
-            var $el = $(ev.currentTarget);
-            if (! $el.hasClass('hovered')) {
-                $el.addClass('hovered');
-            }
-            return true;
-            // console.log($(ev.currentTarget));
-        },
-
-        inlinePerfSmallOut: function (ev) {
-            $(ev.currentTarget).removeClass('hovered');
+            $(document).on('keydown', this.bodyKeyDown);
         },
 
         removeMatchingLinksHighlight: function (ev) {
@@ -54,7 +36,8 @@
             var $link = $(ev.currentTarget);
             var href = $link.attr('href');
             if (typeof href === 'undefined') {
-                console.log('undef', $link);
+                // Link w/o href, ignore
+                // console.log('undef', $link);
                 return;
             }
             // console.log(href);
@@ -126,50 +109,6 @@
             if (typeof attrValue !== 'undefined' && attrValue !== '') {
                 $input.closest('form').submit();
             }
-        },
-
-        leave: function (ev) {
-            this.getInfoArea(ev).text('');
-        },
-
-        rendered: function (ev) {
-            // $('.sparkline')
-        },
-
-        change: function (ev) {
-            var sparkline = ev.sparklines[0],
-                region = sparkline.getCurrentRegionFields(),
-                value = region.y;
-
-            var data = $(ev.target).data();
-            var d = new Date(data.first + region.x * data.interval * 1000);
-            this.getInfoArea(ev).text('[' + d.toLocaleTimeString() + '] ' + region.y);
-        },
-
-        getInfoArea: function (ev) {
-            var $el = $(ev.currentTarget);
-            $parent = $el.closest('td');
-
-            $info = $parent.find('.sparkinfo');
-            if ($info.length === 0) {
-                $info = $('<span class="sparkinfo"></span>');
-                $parent.append($info);
-            }
-
-            return $info;
-        },
-
-        showInfo: function () {
-            $('.overspark').bind('sparklineRegionChange', function (ev) {
-                console.log(ev);
-                return;
-                var sparkline = ev.sparklines[0],
-                    region = sparkline.getCurrentRegionFields(),
-                    value = region.y;
-                $('.mouseoverregion').text("x="+region.x+" y="+region.y);
-            }).bind('mouseleave', function () {
-                $('.mouseoverregion').text('');
-            });
         }
     };
 
