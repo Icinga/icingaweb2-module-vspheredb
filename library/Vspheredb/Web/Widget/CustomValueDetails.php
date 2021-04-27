ff42a885236c66ea619b2134454946dfc4b17bab
Thomas Gelf <thomas@gelf.net>
2021-04-27 11:47:44 +0200

    CustomValueDetails: add demo mode

--- library/Vspheredb/Web/Widget/CustomValueDetails.php
+++ library/Vspheredb/Web/Widget/CustomValueDetails.php
@@ -38,12 +38,22 @@ class CustomValueDetails extends HtmlDocument
         $this->prepend(new SubTitle($this->translate('Custom Values'), 'tags'));
         $values = $object->customValues();
         $this->stripBackupToolCustomValues($values);
+        $demoMode = false;
+        if ($demoMode) {
+            $this->add(NameValueTable::create([
+                'Contact Persons'   => 'John Wayne, Donald Duck',
+                'Application'       => 'WebSphere Application Server',
+                'Installation Date' => '2020-01-02',
+                'Cost Center'       => '48145',
+                'Department'        => 'Web Shop',
+            ]));
+            return;
+        }
+
         if ($values->isEmpty()) {
             $this->add($this->translate('No custom values have been defined'));
         } else {
-            $customValues = new NameValueTable();
-            $customValues->addNameValuePairs($values->toArray());
-            $this->add($customValues);
+            $this->add(NameValueTable::create($values->toArray()));
         }
     }
 
