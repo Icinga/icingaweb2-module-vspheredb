a90d5f088b9aefa0a33665d45b39e5053ec7dcc9
Thomas Gelf <thomas@gelf.net>
2021-11-02 12:08:07 +0100

    CustomFieldsManager: provide mapping helpers

--- library/Vspheredb/MappedClass/CustomFieldsManager.php
+++ library/Vspheredb/MappedClass/CustomFieldsManager.php
@@ -6,4 +6,41 @@ class CustomFieldsManager
 {
     /** @var CustomFieldDef[] */
     public $field;
+
+    protected $map;
+
+    public function mapValues($values)
+    {
+        $map = $this->requireMap();
+        $result = [];
+        /** @var CustomFieldValue $value */
+        foreach ($values as $value) {
+            $key = $value->key;
+            if (isset($map[$key])) {
+                $result[$map[$key]] = $value->value;
+            } else {
+                $result[$key] = $value->value;
+            }
+        }
+
+        return (object) $result;
+    }
+
+    protected function requireMap()
+    {
+        if ($this->map === null) {
+            if (isset($this->field->CustomFieldDef)) { // Mapping goes wrong for arrays of X
+                $fields = $this->field->CustomFieldDef;
+            } else {
+                var_dump($this);
+                die('Custom Fields Manager is now correct');
+            }
+            $this->map = [];
+            foreach ($fields as $field) {
+                $this->map[$field->key] = $field->name;
+            }
+        }
+
+        return $this->map;
+    }
 }
