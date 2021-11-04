7ba33a8590d81ee96dc6f3e04957c48f5379728f
Thomas Gelf <thomas@gelf.net>
2021-11-04 07:35:03 +0100

    DamonController: talk through our socket

--- application/controllers/DaemonController.php
+++ application/controllers/DaemonController.php
@@ -6,8 +6,12 @@ use gipfl\IcingaWeb2\Icon;
 use gipfl\Json\JsonString;
 use gipfl\Web\Widget\Hint;
 use Icinga\Date\DateFormatter;
+use Icinga\Module\Vspheredb\DbObject\VCenterServer;
+use Icinga\Module\Vspheredb\Polling\ServerSet;
+use Icinga\Module\Vspheredb\Web\Table\ControlSocketConnectionsTable;
 use Icinga\Module\Vspheredb\Format;
 use Icinga\Module\Vspheredb\Web\Controller;
+use Icinga\Module\Vspheredb\Web\Table\VsphereApiConnectionTable;
 use Icinga\Module\Vspheredb\Web\Tabs\MainTabs;
 use Icinga\Module\Vspheredb\WebUtil;
 use ipl\Html\Html;
@@ -15,12 +19,28 @@ use ipl\Html\Table;
 
 class DaemonController extends Controller
 {
+    use AsyncControllerHelper;
+
     public function indexAction()
     {
         $this->assertPermission('vspheredb/admin');
-        $this->setAutorefreshInterval(3);
+        $this->setAutorefreshInterval(30);
         $this->addTitle($this->translate('vSphereDB Daemon Status'));
         $this->handleTabs();
+        $this->content()->add([
+            Html::tag('h3', $this->translate('Damon Processes')),
+            $this->prepareDaemonInfo(),
+            Html::tag('h3', $this->translate('Control Socket Connections')),
+            $this->prepareConnectionTable(),
+            Html::tag('h3', $this->translate('vSphere API Connections')),
+            $this->prepareVsphereConnectionTable(),
+            Html::tag('h3', $this->translate('Damon Log Output')),
+            $this->prepareLogWindow()
+        ]);
+    }
+
+    protected function prepareDaemonInfo()
+    {
         $db = $this->db()->getDbAdapter();
         $daemon = $db->fetchRow(
             $db->select()
@@ -28,6 +48,42 @@ class DaemonController extends Controller
                 ->order('ts_last_refresh DESC')
                 ->limit(1)
         );
+
+        if ($daemon) {
+            if ($daemon->ts_last_refresh / 1000 < time() - 10) {
+                return Hint::error(Html::sprintf(
+                    "Daemon keep-alive is outdated in our database, last refresh was %s",
+                    WebUtil::timeAgo($daemon->ts_last_refresh / 1000)
+                ));
+            } else {
+                return $this->prepareProcessTable(JsonString::decode($daemon->process_info));
+            }
+        } else {
+            return Hint::error($this->translate('Daemon is either not running or not connected to the Database'));
+        }
+    }
+
+    protected function prepareProcessTable($processes)
+    {
+        $table = new Table();
+        foreach ($processes as $pid => $process) {
+            $table->add($table::row([
+                [
+                    Icon::create($process->running ? 'ok' : 'warning-empty'),
+                    ' ',
+                    $pid
+                ],
+                $process->command,
+                Format::bytes($process->memory->rss)
+            ]));
+        }
+
+        return $table;
+    }
+
+    protected function prepareLogWindow()
+    {
+        $db = $this->db()->getDbAdapter();
         $lineCount = 1000;
         $logLines = $db->fetchAll($db->select()
             ->from('vspheredb_daemonlog')
@@ -47,32 +103,25 @@ class DaemonController extends Controller
             ], "$tsFormatted: " . $line->message));
         }
 
-        if ($daemon) {
-            if ($daemon->ts_last_refresh / 1000 < time() - 10) {
-                $info = Hint::error(Html::sprintf(
-                    "Daemon keep-alive is outdated, last refresh was %s",
-                    WebUtil::timeAgo($daemon->ts_last_refresh / 1000)
-                ));
-            } else {
-                $processes = json_decode($daemon->process_info);
-                $table = new Table();
-                foreach ($processes as $pid => $process) {
-                    $table->add($table::row([
-                        [
-                            Icon::create($process->running ? 'ok' : 'warning-empty'),
-                            ' ',
-                            $pid
-                        ],
-                        $process->command,
-                        Format::bytes($process->memory->rss)
-                    ]));
-                }
-                $info = $table;
-            }
-        } else {
-            $info = Hint::error($this->translate('Daemon is not running'));
+        return $logWindow;
+    }
+
+    protected function prepareConnectionTable()
+    {
+        try {
+            return new ControlSocketConnectionsTable($this->syncRpcCall('connections.list'));
+        } catch (\Exception $exception) {
+            return Hint::error($exception->getMessage());
         }
-        $this->content()->add([$info, $logWindow]);
+    }
+
+    protected function prepareVsphereConnectionTable()
+    {
+        try {
+            return new VsphereApiConnectionTable($this->syncRpcCall('vsphere.getApiConnections'));
+        } catch (\Exception $exception) {
+            return Hint::error($exception->getMessage());
+        }
     }
 
     protected function handleTabs()
