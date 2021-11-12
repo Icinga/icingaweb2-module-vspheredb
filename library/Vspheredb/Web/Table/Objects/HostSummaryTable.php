343cc0bf37084c8a969385e9f107f661a2f4edbd
Thomas Gelf <thomas@gelf.net>
2021-11-12 16:53:27 +0100

    HostSummaryTable: re-organize tables
    Remove parts once shown elsewhere

--- library/Vspheredb/Web/Table/Objects/HostSummaryTable.php
+++ library/Vspheredb/Web/Table/Objects/HostSummaryTable.php
@@ -81,8 +81,7 @@ abstract class HostSummaryTable extends ObjectsTable
 
             return [
                 $link,
-                Html::tag('br'),
-                Html::tag('small', $this->renderHostSummaries($row)),
+                $this->renderHostSummaries($row),
             ];
         });
     }
@@ -96,55 +95,10 @@ abstract class HostSummaryTable extends ObjectsTable
     {
         $this->addAvailableColumns([
             $this->createGroupingColumn(),
-
-            $this->createColumn('cpu', $this->translate('CPU'), [
-                'used_mhz'  => 'SUM(hqs.overall_cpu_usage)',
-                'total_mhz' => 'SUM(h.hardware_cpu_cores * h.hardware_cpu_mhz)',
-            ])->setRenderer(function ($row) {
-                $bar = new CpuUsage($row->used_mhz, $row->total_mhz);
-                if ($this->hasChosenColumn('overall_cpu_usage') || $this->hasChosenColumn('hardware_cpu_mhz')) {
-                    $bar->showLabels(false);
-                }
-                return $bar;
-            })->setSortExpression(
-                'SUM(hqs.overall_cpu_usage) / SUM(h.hardware_cpu_cores * h.hardware_cpu_mhz)'
-            )->setDefaultSortDirection('DESC'),
-            $this->createColumn('overall_cpu_usage', $this->translate('Used'), 'SUM(hqs.overall_cpu_usage)')
+            $this->createColumn('cnt_hosts', $this->translate('Hosts'), 'COUNT(*)')
                 ->setRenderer(function ($row) {
-                    return Format::mhz($row->overall_cpu_usage);
-                })->setDefaultSortDirection('DESC'),
-            $this->createColumn(
-                'hardware_cpu_mhz',
-                $this->translate('Capacity'),
-                'SUM(h.hardware_cpu_cores * h.hardware_cpu_mhz)'
-            )->setRenderer(function ($row) {
-                return Format::mhz($row->hardware_cpu_mhz);
-            })->setDefaultSortDirection('DESC'),
-            $this->createColumn('hardware_cpu_cores', $this->translate('Cores'), 'SUM(h.hardware_cpu_cores)')
-                ->setDefaultSortDirection('DESC'),
-
-            $this->createColumn('memory', $this->translate('Memory'), [
-                'used_mb'  => 'SUM(hqs.overall_memory_usage_mb)',
-                'total_mb' => 'SUM(h.hardware_memory_size_mb)',
-            ])->setRenderer(function ($row) {
-                $bar = new MemoryUsage($row->used_mb, $row->total_mb);
-                if ($this->hasChosenColumn('overall_memory_usage') || $this->hasChosenColumn('hardware_memorymb')) {
-                    $bar->showLabels(false);
-                }
-                return $bar;
-            })->setSortExpression(
-                'AVG(hqs.overall_memory_usage_mb / h.hardware_memory_size_mb)'
-            )->setDefaultSortDirection('DESC'),
-            $this->createColumn('overall_memory_usage', $this->translate('Used'), 'SUM(hqs.overall_memory_usage_mb)')
-                ->setRenderer(function ($row) {
-                    return Format::mBytes($row->overall_memory_usage);
-                })->setDefaultSortDirection('DESC'),
-            $this->createColumn('hardware_memorymb', $this->translate('Capacity'), 'SUM(h.hardware_memory_size_mb)')
-                ->setRenderer(function ($row) {
-                    return Format::mBytes($row->hardware_memorymb);
-                })->setDefaultSortDirection('DESC'),
-
-            $this->createColumn('cnt_hosts', 'Hosts', 'COUNT(*)')
+                    return Html::tag('td', ['style' => 'text-align: right'], $row->cnt_hosts);
+                })
                 ->setDefaultSortDirection('DESC'),
             $this->createColumn(
                 'hosts_status',
@@ -174,6 +128,55 @@ abstract class HostSummaryTable extends ObjectsTable
                 "SUM(CASE WHEN ho.overall_status = 'gray' THEN 1 ELSE 0 END)",
                 "SUM(CASE WHEN ho.overall_status = 'green' THEN 1 ELSE 0 END)",
             ])->setDefaultSortDirection('DESC'),
+            $this->createColumn('cpu', $this->translate('CPU'), [
+                'used_mhz'  => 'SUM(hqs.overall_cpu_usage)',
+                'total_mhz' => 'SUM(h.hardware_cpu_cores * h.hardware_cpu_mhz)',
+            ])->setRenderer(function ($row) {
+                $bar = new CpuUsage($row->used_mhz, $row->total_mhz);
+                if ($this->hasChosenColumn('overall_cpu_usage') || $this->hasChosenColumn('hardware_cpu_mhz')) {
+                    $bar->showLabels(false);
+                }
+                return $bar;
+            })->setSortExpression(
+                'SUM(hqs.overall_cpu_usage) / SUM(h.hardware_cpu_cores * h.hardware_cpu_mhz)'
+            )->setDefaultSortDirection('DESC'),
+            $this->createColumn('overall_cpu_usage', $this->translate('Used'), 'SUM(hqs.overall_cpu_usage)')
+                ->setRenderer(function ($row) {
+                    return Format::mhz($row->overall_cpu_usage);
+                })->setDefaultSortDirection('DESC'),
+            $this->createColumn(
+                'hardware_cpu_mhz',
+                $this->translate('Capacity'),
+                'SUM(h.hardware_cpu_cores * h.hardware_cpu_mhz)'
+            )->setRenderer(function ($row) {
+                return Format::mhz($row->hardware_cpu_mhz);
+            })->setDefaultSortDirection('DESC'),
+            $this->createColumn('hardware_cpu_cores', $this->translate('Cores'), 'SUM(h.hardware_cpu_cores)')
+                ->setRenderer(function ($row) {
+                    return Html::tag('td', ['style' => 'text-align: right'], $row->hardware_cpu_cores);
+                })
+                ->setDefaultSortDirection('DESC'),
+
+            $this->createColumn('memory', $this->translate('Memory'), [
+                'used_mb'  => 'SUM(hqs.overall_memory_usage_mb)',
+                'total_mb' => 'SUM(h.hardware_memory_size_mb)',
+            ])->setRenderer(function ($row) {
+                $bar = new MemoryUsage($row->used_mb, $row->total_mb);
+                if ($this->hasChosenColumn('overall_memory_usage') || $this->hasChosenColumn('hardware_memorymb')) {
+                    $bar->showLabels(false);
+                }
+                return $bar;
+            })->setSortExpression(
+                'AVG(hqs.overall_memory_usage_mb / h.hardware_memory_size_mb)'
+            )->setDefaultSortDirection('DESC'),
+            $this->createColumn('overall_memory_usage', $this->translate('Used'), 'SUM(hqs.overall_memory_usage_mb)')
+                ->setRenderer(function ($row) {
+                    return Format::mBytes($row->overall_memory_usage);
+                })->setDefaultSortDirection('DESC'),
+            $this->createColumn('hardware_memorymb', $this->translate('Capacity'), 'SUM(h.hardware_memory_size_mb)')
+                ->setRenderer(function ($row) {
+                    return Format::mBytes($row->hardware_memorymb);
+                })->setDefaultSortDirection('DESC'),
 
             /*
              // Not yet, this was an early prototype based no monitoring vars
@@ -204,26 +207,39 @@ abstract class HostSummaryTable extends ObjectsTable
     protected function renderHostSummaries($row)
     {
         $params = $this->getFilterParams($row);
-        $hosts = [
-            Link::create(
+
+        $result = [];
+        if (! $this->hasChosenColumn('cnt_hosts')) {
+            $result[] = Link::create(
                 (int) $row->hosts_cnt === 1
                     ? $this->translate('1 Host')
                     : sprintf($this->translate('%d Hosts'), $row->hosts_cnt),
                 'vspheredb/hosts',
                 $params
-            ),
-            ': '
-        ];
-        foreach (['red', 'yellow', 'gray', 'green'] as $state) {
-            $column = "hosts_cnt_overall_$state";
-            if ($row->$column > 0) {
-                $hosts[] = Link::create($row->$column, 'vspheredb/hosts', $params + [
-                        'overall_status' => $state
-                    ], ['class' => ['state', $state]]);
+            );
+        }
+
+        if (! $this->hasChosenColumn('hosts_status')) {
+            if (! empty($result)) {
+                $result[] = ': ';
+            }
+            foreach (['red', 'yellow', 'gray', 'green'] as $state) {
+                $column = "hosts_cnt_overall_$state";
+                if ($row->$column > 0) {
+                    $result[] = Link::create($row->$column, 'vspheredb/hosts', $params + [
+                            'overall_status' => $state
+                        ], ['class' => ['state', $state]]);
+                }
             }
         }
 
-        return $hosts;
+        if (empty($result)) {
+            return null;
+        }
+        return [
+            Html::tag('br'),
+            Html::tag('small', $result),
+        ];
     }
 
     public function getDefaultColumnNames()
