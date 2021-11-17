2b43c5a1cd3c02f409eacecfc59edf729f9ec15f
F <58272808+ascii42@users.noreply.github.com>
2021-11-17 11:35:03 +0100

    Upate Fujitsu and HP/E Vendor Images

--- library/Vspheredb/Web/Table/Object/known-vendor-model-images.php
+++ library/Vspheredb/Web/Table/Object/known-vendor-model-images.php
@@ -21,38 +21,45 @@ return  [
             . 'product_images/dell_enterprise_products/enterprise_systems/'
             . 'poweredge/r_series/r940/global_spi/enterprise-servers-poweredge-dellemc-'
             . 'per940-bezel-lcd-2-if-on-left-relativesize-500-ng.psd?fmt=jpg',
-        'PowerEdge R6515' => 'https://i.dell.com/is/image/DellContent//content/dam/global-site-design/'
-            . 'product_images/dell_enterprise_products/enterprise_systems/'
-            . 'poweredge/poweredge_amd/pdp/poweredge-r6515-lf-555x200-pdp.jpg?fmt=jpg',
-        'PowerEdge R7515' => 'https://i.dell.com/is/image/DellContent//content/dam/global-site-design/'
-            . 'product_images/dell_enterprise_products/enterprise_systems/'
-            . 'poweredge/c6525/global_spi/ng/enterprise-server-poweredge-r7515-lf-relativesize-500-ng.psd?fmt=jpg',
-        'PowerEdge R7525' => 'https://i.dell.com/is/image/DellContent//content/dam/global-site-design/'
-            . 'product_images/dell_enterprise_products/enterprise_systems/'
-            . 'poweredge/poweredge_r7525/global_spi/ng/enterprise-servers-poweredge-r7525-lf-relativesize-500-ng.psd?fmt=jpg',
     ],
     'HPE' => [
         'Synergy 480 Gen10' => 'https://support.hpe.com/hpesc/public/api/document/a00020236en_us/'
             . 'attachment/a00039214.png/',
+        'Synergy 480 Gen10 w/ PCIe Exp Mod' => 'https://support.hpe.com/hpesc/public/api/document/a00020236en_us/'
+            . 'attachment/a00039214.png/',
         'ProLiant DL380 Gen10' => 'https://support.hpe.com/hpesc/public/api/document/a00019682en_us/'
             . 'attachment/a00050167.png/',
+        'ProLiant BL460c Gen10' => 'https://support.hpe.com/hpesc/public/api/document/a00019527en_us/'
+            . 'attachment/a00050263.png/',
         'ProLiant DL560 Gen10' => 'https://support.hpe.com/hpesc/public/api/document/product-image/i00022852/payload',
         'ProLiant DL580 Gen10' => 'https://support.hpe.com/hpesc/public/api/document/product-image/i00027400/payload',
-
+        'ProLiant DL360 Gen10' => 'https://support.hpe.com/hpesc/public/api/document/product-image/i00023792/payload',
     ],
     'HP' => [
         'ProLiant DL380 Gen9' => 'https://support.hpe.com/hpesc/public/api/document/c04438844/'
             . 'attachment/c04444813.gif/',
+        'ProLiant BL460c Gen9' => 'https://support.hpe.com/hpesc/public/api/document/c04434541/'
+            . 'attachment/a00110345.gif/',
         'ProLiant DL580 Gen9' => 'https://support.hpe.com/hpesc/public/api/document/c04683220/'
             . 'attachment/c04683239.gif/',
+        'ProLiant DL360 Gen9' => 'https://support.hpe.com/hpesc/public/api/document/c04442953/'
+            . '/attachment/a00076043.png/',
     ],
     'Lenovo' => [
         'ThinkAgile VX 1U Node -[7Y93CTO4WW]-' => 'https://lenovopress.com/assets/images/LP0932/VX-1U-overview.png',
         'ThinkSystem SR850 -[7X19CTO1WW]-' => 'https://lenovopress.com/assets/images/LP1301/'
             . 'ThinkSystem-SR850-V2-500px.png',
     ],
+    'FUJITSU' => [
+        'PRIMERGY CX2560 M5' => 'https://www.fujitsu.com/de/imagesgig5/W-DK43300_tcm20-4285182_tcm20-5309118-32.png',
+        'PRIMERGY RX2540 M1' => 'https://www.fujitsu.com/global/imagesgig5/W-DK72231_tcm100-5877559_tcm100-5309118-32.png',
+        'PRIMERGY RX2540 M2' => 'https://www.fujitsu.com/de/Images/W-DK42852_tcm20-3057159.png',
+        'PRIMERGY RX2540 M3' => 'https://www.fujitsu.com/de/Images/W-DK42852_tcm20-3057159.png',
+        'PRIMERGY RX2540 M4' => 'https://www.fujitsu.com/de/Images/W-DK42852_tcm20-3057159.png',
+    ],
     'IBM' => [
         '/^(?:System )?x3850 X6/' => 'https://lenovopress.com/assets/images/tips1084/0.212C.jpg',
+        'IBM System x3650 M4: -[7915B3G]-' => 'https://www.ibm.com/common/ssi/GIF/ALET/JX3650M4.JPG',
         // Variants:
         //    System x3850 X6 -[3837CT0]-
         //    x3850 X6 -[3837ZMQ]-
