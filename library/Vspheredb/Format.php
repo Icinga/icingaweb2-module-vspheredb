880e091fea080024c579da9555dab448c2523174
Thomas Gelf <thomas@gelf.net>
2022-05-05 09:51:58 +0200

    Format: fix byte formatting for 0 < val < 1

--- library/Vspheredb/Format.php
+++ library/Vspheredb/Format.php
@@ -8,9 +8,6 @@ class Format
     {
         $base = 1024;
         $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-        if ($value == 0) {
-            return sprintf('%.3G %s', 0, $units[0]);
-        }
 
         $sign = '';
         if ($value < 0) {
@@ -18,8 +15,8 @@ class Format
             $sign = '-';
         }
 
-        // 1000 instead of base is to fit into %.3G
-        $pow = floor(log($value, 1000));
+        // max() deals with $value < 1, 1000 instead of base is to fit into %.3G
+        $pow = max(0, floor(log($value, 1000)));
         $result = $value / pow($base, $pow);
 
         // Problem: %.3G is 0.978 for $value = 1001 / 1024, but we want to see 0.98
