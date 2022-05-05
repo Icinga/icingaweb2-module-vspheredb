8f1787add70f8e96958d79f4491f31d68d1e8782
Thomas Gelf <thomas@gelf.net>
2022-05-05 09:50:45 +0200

    Format: fix IEC byte output for 1000 < val < 1024
    fixes #367

--- library/Vspheredb/Format.php
+++ library/Vspheredb/Format.php
@@ -18,9 +18,17 @@ class Format
             $sign = '-';
         }
 
-        $pow = floor(log($value, $base));
+        // 1000 instead of base is to fit into %.3G
+        $pow = floor(log($value, 1000));
         $result = $value / pow($base, $pow);
-        return sprintf('%s%.3G %s', $sign, $result, $units[$pow]);
+
+        // Problem: %.3G is 0.978 for $value = 1001 / 1024, but we want to see 0.98
+        $output = sprintf('%.3G', $result);
+        if (preg_match('/^0[,.]/', $output)) {
+            $output = sprintf('%.3G', round((float) $output, 2));
+        }
+
+        return sprintf('%s%s %s', $sign, $output, $units[$pow]);
     }
 
     public static function mBytes($value)
