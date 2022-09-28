934f6d6651cb7c7fccaf0b45b03e6b98e6bd78ee
Thomas Gelf <thomas@gelf.net>
2022-09-28 21:04:17 +0200

    MemoryUsageHelper: extract logic useful for other
    ...definitions

--- library/Vspheredb/Monitoring/Rule/Definition/DiskUsageRuleDefinition.php
+++ library/Vspheredb/Monitoring/Rule/Definition/DiskUsageRuleDefinition.php
@@ -3,12 +3,9 @@
 namespace Icinga\Module\Vspheredb\Monitoring\Rule\Definition;
 
 use Icinga\Module\Vspheredb\DbObject\BaseDbObject;
-use Icinga\Module\Vspheredb\Monitoring\CheckPluginState;
-use Icinga\Module\Vspheredb\Monitoring\CheckPluginState as State;
 use Icinga\Module\Vspheredb\Monitoring\Rule\Enum\ObjectType;
 use Icinga\Module\Vspheredb\Monitoring\Rule\Settings;
 use Icinga\Module\Vspheredb\Monitoring\SingleCheckResult;
-use Icinga\Util\Format;
 
 class DiskUsageRuleDefinition extends MonitoringRuleDefinition
 {
@@ -50,49 +47,8 @@ class DiskUsageRuleDefinition extends MonitoringRuleDefinition
                 return null;
             }
         }
-        $percentFree = $disk->free_space / $disk->capacity * 100;
-        $state = new CheckPluginState();
-        $output = sprintf(
-            '%s out of %s (%.2F%%) free on %s',
-            Format::bytes($disk->free_space, Format::STANDARD_IEC),
-            Format::bytes($disk->capacity, Format::STANDARD_IEC),
-            $percentFree,
-            $disk->disk_path
-        );
 
-        $percentState = new State();
-        $min = $settings->get('warning_if_less_than_percent_free');
-        if ($min && ($percentFree < (float) $min)) {
-            $percentState->raiseState(State::WARNING);
-        }
-        $min = $settings->get('critical_if_less_than_percent_free');
-        if ($min && ($percentFree < (float) $min)) {
-            $percentState->raiseState(State::CRITICAL);
-        }
-
-        $mbState = new State();
-        $mbFree = $disk->free_space / (1024 * 1024);
-        $min = $settings->get('warning_if_less_than_mbytes_free');
-        if ($min && ($mbFree < (float) $min)) {
-            $mbState->raiseState(State::WARNING);
-        }
-        $min = $settings->get('critical_if_less_than_mbytes_free');
-        if ($min && ($mbFree < (float) $min)) {
-            $mbState->raiseState(State::CRITICAL);
-        }
-
-        if ($mbState->isProblem() && $percentState->isProblem()) {
-            if ($settings->get('threshold_precedence') === 'worst') {
-                $state->raiseState(State::getWorst($percentState, $mbState));
-            } else {
-                $state->raiseState(State::getBest($percentState, $mbState));
-            }
-        } else {
-            $state->raiseState($percentState);
-            $state->raiseState($mbState);
-        }
-
-        return new SingleCheckResult($state, $output);
+        return MemoryUsageHelper::prepareState($settings, $disk->free_space, $disk->capacity, $disk->disk_path);
     }
 
     protected function filterMatchesPath(string $filterString, string $path): bool
@@ -163,30 +119,6 @@ class DiskUsageRuleDefinition extends MonitoringRuleDefinition
                 'label' => $this->translate('Ignore specific disks'),
                 'placeholder' => 'e.g. C:, */volume-subpaths/*|/var/lib/kubelet/*',
             ]],
-            'threshold_precedence' => ['select', [
-                'label' => $this->translate('Threshold Precedence'),
-                'options' => [
-                    null => $this->translate('- please choose -'),
-                    'best_wins' => $this->translate('Better threshold wins'),
-                    'worst_wins' => $this->translate('Worse threshold wins'),
-                ],
-            ]],
-            'warning_if_less_than_percent_free' => ['number', [
-                'label' => $this->translate('Raise Warning with less then X percent free'),
-                'placeholder' => '5',
-            ]],
-            'critical_if_less_than_percent_free' => ['number', [
-                'label' => $this->translate('Raise Critical with less then X percent free'),
-                'placeholder' => '2',
-            ]],
-            'warning_if_less_than_mbytes_free' => ['number', [
-                'label' => $this->translate('Raise Warning with less then X MBytes free'),
-                'placeholder' => '500',
-            ]],
-            'critical_if_less_than_mbytes_free' => ['number', [
-                'label' => $this->translate('Raise Critical with less then X MBytes free'),
-                'placeholder' => '100',
-            ]],
-        ];
+        ] + MemoryUsageHelper::getParameters();
     }
 }
---
+++ library/Vspheredb/Monitoring/Rule/Definition/MemoryUsageHelper.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Icinga\Module\Vspheredb\Monitoring\Rule\Definition;
+
+use gipfl\Translation\StaticTranslator;
+use Icinga\Module\Vspheredb\Monitoring\CheckPluginState as State;
+use Icinga\Module\Vspheredb\Monitoring\Rule\Settings;
+use Icinga\Module\Vspheredb\Monitoring\SingleCheckResult;
+use Icinga\Util\Format;
+
+class MemoryUsageHelper
+{
+    public const MEGA_BYTE = 1024 * 1024;
+
+    public static function prepareState(
+        Settings $settings,
+        int      $free,
+        int      $capacity,
+        ?string  $instanceName = null
+    ): SingleCheckResult {
+        $percentFree = $free / $capacity * 100;
+        $state = new State();
+        $output = sprintf(
+            '%s out of %s (%.2F%%) free',
+            Format::bytes($free, Format::STANDARD_IEC),
+            Format::bytes($capacity, Format::STANDARD_IEC),
+            $percentFree
+        );
+        if ($instanceName) {
+            $output = "$instanceName has $output";
+        }
+
+        $percentState = new State();
+        $min = $settings->get('warning_if_less_than_percent_free');
+        if ($min && ($percentFree < (float) $min)) {
+            $percentState->raiseState(State::WARNING);
+        }
+        $min = $settings->get('critical_if_less_than_percent_free');
+        if ($min && ($percentFree < (float) $min)) {
+            $percentState->raiseState(State::CRITICAL);
+        }
+
+        $mbState = new State();
+        $mbFree = $free / self::MEGA_BYTE;
+        $min = $settings->get('warning_if_less_than_mbytes_free');
+        if ($min && ($mbFree < (float) $min)) {
+            $mbState->raiseState(State::WARNING);
+        }
+        $min = $settings->get('critical_if_less_than_mbytes_free');
+        if ($min && ($mbFree < (float) $min)) {
+            $mbState->raiseState(State::CRITICAL);
+        }
+
+        if ($mbState->isProblem() && $percentState->isProblem()) {
+            if ($settings->get('threshold_precedence') === 'worst') {
+                $state->raiseState(State::getWorst($percentState, $mbState));
+            } else {
+                $state->raiseState(State::getBest($percentState, $mbState));
+            }
+        } else {
+            $state->raiseState($percentState);
+            $state->raiseState($mbState);
+        }
+
+        return new SingleCheckResult($state, $output);
+    }
+
+    public static function getParameters(): array
+    {
+        $t = StaticTranslator::get();
+        return [
+            'threshold_precedence' => ['select', [
+                'label' => $t->translate('Threshold/State Precedence'),
+                'options' => [
+                    null => $t->translate('- please choose -'),
+                    'best_wins'  => $t->translate('Better state wins'),
+                    'worst_wins' => $t->translate('Worse state wins'),
+                ],
+            ]],
+            'warning_if_less_than_percent_free' => ['number', [
+                'label' => $t->translate('Raise Warning with less then X percent free'),
+                'placeholder' => '5',
+            ]],
+            'critical_if_less_than_percent_free' => ['number', [
+                'label' => $t->translate('Raise Critical with less then X percent free'),
+                'placeholder' => '2',
+            ]],
+            'warning_if_less_than_mbytes_free' => ['number', [
+                'label' => $t->translate('Raise Warning with less then X MBytes free'),
+                'placeholder' => '500',
+            ]],
+            'critical_if_less_than_mbytes_free' => ['number', [
+                'label' => $t->translate('Raise Critical with less then X MBytes free'),
+                'placeholder' => '100',
+            ]],
+        ];
+    }
+}
