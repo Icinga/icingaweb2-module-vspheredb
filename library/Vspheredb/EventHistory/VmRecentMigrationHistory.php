0f2af084ea981beea85151a27bd1ea538f983680
Thomas Gelf <thomas@gelf.net>
2022-10-05 15:57:20 +0200

    VmRecentMigrationHistory: early quoting
    Avoids potential bugs in the underlying DB library

--- library/Vspheredb/EventHistory/VmRecentMigrationHistory.php
+++ library/Vspheredb/EventHistory/VmRecentMigrationHistory.php
@@ -2,6 +2,7 @@
 
 namespace Icinga\Module\Vspheredb\EventHistory;
 
+use Icinga\Module\Vspheredb\Db\DbUtil;
 use Icinga\Module\Vspheredb\DbObject\VirtualMachine;
 
 class VmRecentMigrationHistory
@@ -20,7 +21,7 @@ class VmRecentMigrationHistory
     {
         $query = $this->db->select()
             ->from('vm_event_history', 'COUNT(*)')
-            ->where('vm_uuid = ?', $this->vm->get('uuid'))
+            ->where('vm_uuid = ?', DbUtil::quoteBinaryCompat($this->vm->get('uuid'), $this->db))
             ->where('ts_event_ms > ?', (time() - 86400 * 4) * 1000)
             ->where('event_type IN (?)', [
                 'VmBeingMigratedEvent',
