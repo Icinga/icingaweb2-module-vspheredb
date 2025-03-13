30b12ab1e5d887337694aa5d6fb592a00968f39f
Johannes Rauh <johannes.rauh@icinga.com>
2025-03-13 13:59:55 +0100

    Add more indexes

---
+++ schema/mysql-migrations/upgrade_62.sql
@@ -0,0 +1,8 @@
+CREATE INDEX host_name ON host_system (host_name);
+CREATE INDEX template ON virtual_machine (template);
+CREATE INDEX vcenter_uuid ON datastore (vcenter_uuid);
+CREATE INDEX vcenter_uuid ON host_system (vcenter_uuid);
+CREATE INDEX vcenter_uuid ON virtual_machine (vcenter_uuid);
+CREATE INDEX vcenter_uuid ON object (vcenter_uuid);
+CREATE INDEX vcenter_uuid ON host_sensor (vcenter_uuid);
+CREATE INDEX vcenter_uuid ON host_pci_device (vcenter_uuid);
