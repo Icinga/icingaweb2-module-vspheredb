e073e0a58e5faf377ffe6a2059df454d688de9ca
Johannes Rauh <johannes.rauh@icinga.com>
2026-02-19 10:33:59 +0100

    Replace abandoned library `clue/block-react`
    Replace `Clue\Block\React::await()` with `React\Async::await()`.
    
    The `React\Async::await()` function does not support the `$timeout` param, so if
    it has been set the `$promise` will be created out of
    `React\Promise\Timer::timeout()`.
    
    (cherry picked from commit 341d64115bcefb9233801a07e539c02b83aea2e2)

--- application/controllers/AsyncControllerHelper.php
+++ application/controllers/AsyncControllerHelper.php
@@ -2,10 +2,12 @@
 
 namespace Icinga\Module\Vspheredb\Controllers;
 
-use Clue\React\Block;
 use Icinga\Module\Vspheredb\Configuration;
 use Icinga\Module\Vspheredb\Daemon\RemoteClient;
 use React\EventLoop\Factory as Loop;
+
+use function React\Async\await;
+use function React\Promise\Timer\timeout;
 
 trait AsyncControllerHelper
 {
@@ -16,7 +18,7 @@ trait AsyncControllerHelper
 
     protected function syncRpcCall($method, $params = [], $timeout = 30)
     {
-        return Block\await($this->remoteClient()->request($method, $params), $this->loop(), $timeout);
+        return await(timeout($this->remoteClient()->request($method, $params), $timeout));
     }
 
     /**
